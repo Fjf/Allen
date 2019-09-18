6066c4b5f1980343a88f745868659eb82e551e73
Roel Aaij <roel.aaij@cern.ch>
2019-09-18 16:28:56 +0200

    Update BlockHeader for reading and add MEP reading test

--- mdf/include/eb_header.hpp
+++ mdf/include/eb_header.hpp
@@ -6,8 +6,8 @@
 namespace EB {
 struct Header {
 
-  unsigned short source_id = 0;
-  unsigned short version = 0;
+  uint16_t source_id = 0;
+  uint16_t version = 0;
 
 };
 
@@ -15,39 +15,66 @@ struct BlockHeader {
 
   BlockHeader() = default;
 
-  void init(uint64_t evid, uint16_t nf)
+  BlockHeader(uint64_t evid, uint16_t nf)
+    : event_id{evid}, n_frag{nf}
   {
-    event_id = evid;
-    n_frag = nf;
-    resize(types);
-    resize(sizes);
-    block_size = sizeof(event_id) + sizeof(n_frag) + sizeof(reserved)
-      + types.size() * sizeof(decltype(types)::value_type)
-      + sizes.size() * sizeof(decltype(sizes)::value_type);
+    resize(types, m_types);
+    resize(sizes, m_sizes);
   };
+
+  BlockHeader(char const* data)
+  {
+    auto set = [&data](auto& t) {
+                 using t_t = std::remove_reference_t<decltype(t)>;
+                 t = *reinterpret_cast<t_t const*>(data);
+                 data += sizeof(t_t);
+               };
+    set(event_id);
+    set(n_frag);
+    set(reserved);
+    set(block_size);
+    types = make_span<unsigned char>(data);
+    data += types.size_bytes();
+    sizes = make_span<uint16_t>(data);
+  }
 
   uint64_t event_id = 0;
   uint16_t n_frag = 0;
   uint16_t reserved = 0;
-  uint32_t block_size;
-  std::vector<unsigned char> types;
-  std::vector<unsigned short> sizes;
+  uint32_t block_size = 0;
+  gsl::span<unsigned char> types;
+  gsl::span<uint16_t> sizes;
 
   uint32_t header_size() const {
     return (sizeof(event_id) + sizeof(n_frag) + sizeof(reserved) + sizeof(block_size) +
-            types.size() + sizes.size());
+            + types.size_bytes()
+            + sizes.size_bytes());
   }
 
 private:
 
+  template<typename T>
+  gsl::span<T> make_span(char const* d) {
+    return {const_cast<T*>(reinterpret_cast<T const*>(d)), padded_size<T>()};
+  }
+
   template <typename T>
-  void resize(std::vector<T>& cont)
-  {
+  size_t padded_size() {
     auto p32 = sizeof(int32_t) / sizeof(T);
-    auto size = (n_frag % p32) == 0 ? n_frag : (n_frag / p32 + 1) * p32;
+    return (n_frag % p32) == 0 ? n_frag : (n_frag / p32 + 1) * p32;
+  }
+
+  template <typename T>
+  void resize(gsl::span<T>& view, std::vector<T>& cont)
+  {
+    auto size = padded_size<T>();
     cont.resize(size);
     cont.assign(size, 0);
+    view = gsl::span<T>{&cont[0], size};
   }
+
+  std::vector<unsigned char> m_types;
+  std::vector<uint16_t> m_sizes;
 
 };
 }
---
+++ mdf/test/read_mep.cpp
@@ -0,0 +1,103 @@
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <unordered_set>
+#include <map>
+#include <cassert>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include "raw_bank.hpp"
+#include "read_mdf.hpp"
+#include "eb_header.hpp"
+#include "Logger.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  if (argc != 3) {
+    cout << "usage: test_read file.mdf n_mep" << endl;
+    return -1;
+  }
+
+  string filename = {argv[1]};
+  size_t n_meps = atol(argv[2]);
+
+  // Some storage for reading the events into
+  LHCb::MDFHeader header;
+  vector<char> read_buffer(1024 * 1024, '\0');
+
+  bool eof = false, error = false;
+
+
+  int input = ::open(filename.c_str(), O_RDONLY);
+  if (input != -1) {
+    info_cout << "Opened " << filename << "\n";
+  }
+  else {
+    cerr << "Failed to open file " << filename << " " << strerror(errno) << "\n";
+    return -1;
+  }
+
+  char header_buffer[100];
+  LHCb::MDFHeader* mdf_header = reinterpret_cast<LHCb::MDFHeader*>(header_buffer);
+
+  vector<char> data;
+
+  size_t i_mep = 0;
+  while (!eof && i_mep++ < n_meps) {
+
+    ssize_t n_bytes = ::read(input, header_buffer, sizeof(LHCb::MDFHeader));
+    if (n_bytes <= 0) {
+      cerr << "Failed to read header " << strerror(errno) << "\n";
+    }
+    uint header_version = mdf_header->headerVersion();
+    auto hdr_size = LHCb::MDFHeader::sizeOf(header_version);
+    assert((hdr_size - sizeof(LHCb::MDFHeader)) == mdf_header->subheaderLength());
+    // read subheader
+    n_bytes = ::read(input, header_buffer + sizeof(LHCb::MDFHeader), mdf_header->subheaderLength());
+    if (n_bytes <= 0) {
+      cerr << "Failed to read subheader " << strerror(errno) << "\n";
+    }
+
+    auto data_size = mdf_header->size();
+    data.resize(data_size);
+    n_bytes = ::read(input, &data[0], data_size);
+    if (n_bytes <= 0) {
+      cerr << "Failed to read data of size " << mdf_header->size() << " " << strerror(errno) << "\n";
+    }
+
+    gsl::span<char const> data_span{data.data(), data_size};
+    auto const* d = data_span.begin();
+    cout << "data size " << data_size << "\n";
+    while(d != data_span.end()) {
+      auto eb_header = reinterpret_cast<EB::Header const*>(d);
+      EB::BlockHeader const block_header{d + sizeof(EB::Header)};
+      char const* fragment_data = d + sizeof(EB::Header) + block_header.header_size();
+      char const* fragment_end = fragment_data + block_header.block_size;
+
+      cout << "fragment source_id " << std::setw(4) << eb_header->source_id
+           << " version: " << std::setw(2) << eb_header->version
+           << " event_id: " << std::setw(6) << block_header.event_id
+           << " packing: " << std::setw(4) << block_header.n_frag
+           << " size: " << std::setw(5) << block_header.block_size
+           << " type: " << std::setw(3) << int(block_header.types[0]);
+      for (size_t i = 0; i < block_header.n_frag; ++i) {
+        cout << " bsize " << std::setw(3) << i << ": " << std::setw(4) << block_header.sizes[i];
+      }
+      cout << std::setw(10) << fragment_end - data_span.data();
+      cout << "\n";
+
+      d = fragment_end;
+    }
+
+  }
+
+  return 0;
+}
