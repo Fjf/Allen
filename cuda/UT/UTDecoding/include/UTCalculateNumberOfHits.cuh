55fe528e1374e3f3033f98f8cbcd019c26be21ec
Roel Aaij <roel.aaij@cern.ch>
2019-10-16 14:33:42 +0200

    MEP layout decoding kernel for UTCalculateNumberOfHits

--- cuda/UT/UTDecoding/include/UTCalculateNumberOfHits.cuh
+++ cuda/UT/UTDecoding/include/UTCalculateNumberOfHits.cuh
@@ -17,5 +17,24 @@ __global__ void ut_calculate_number_of_hits(
 
 ALGORITHM(
   ut_calculate_number_of_hits,
-  ut_calculate_number_of_hits_t,
+  ut_calculate_number_of_hits_allen_t,
   ARGUMENTS(dev_ut_raw_input, dev_ut_raw_input_offsets, dev_ut_hit_offsets, dev_event_list))
+
+__global__ void ut_calculate_number_of_hits_mep(
+  const char* dev_ut_raw_input,
+  const uint32_t* dev_ut_raw_input_offsets,
+  const char* ut_boards,
+  const uint* dev_ut_region_offsets,
+  const uint* dev_unique_x_sector_layer_offsets,
+  const uint* dev_unique_x_sector_offsets,
+  uint32_t* dev_ut_hit_offsets,
+  const uint* dev_event_list);
+
+ALGORITHM(
+  ut_calculate_number_of_hits_mep,
+  ut_calculate_number_of_hits_mep_t,
+  ARGUMENTS(dev_ut_raw_input, dev_ut_raw_input_offsets, dev_ut_hit_offsets, dev_event_list))
+
+XOR_ALGORITHM(ut_calculate_number_of_hits_mep_t,
+              ut_calculate_number_of_hits_allen_t,
+              ut_calculate_number_of_hits_t)
--- cuda/UT/UTDecoding/src/UTCalculateNumberOfHits.cu
+++ cuda/UT/UTDecoding/src/UTCalculateNumberOfHits.cu
@@ -1,5 +1,33 @@
 #include "UTCalculateNumberOfHits.cuh"
 
+__device__ void calculate_number_of_hits(
+  uint const* dev_ut_region_offsets,
+  uint const* dev_unique_x_sector_offsets,
+  uint32_t* hit_offsets,
+  UTBoards const& boards,
+  UTRawBank const& raw_bank)
+{
+  const uint32_t m_nStripsPerHybrid = boards.stripsPerHybrids[raw_bank.sourceID];
+
+  for (uint32_t i = threadIdx.y; i < raw_bank.number_of_hits; i += blockDim.y) {
+    const uint32_t channelID = (raw_bank.data[i] & UT::Decoding::chan_mask) >> UT::Decoding::chan_offset;
+    const uint32_t index = channelID / m_nStripsPerHybrid;
+    const uint32_t fullChanIndex = raw_bank.sourceID * UT::Decoding::ut_number_of_sectors_per_board + index;
+    const uint32_t station = boards.stations[fullChanIndex] - 1;
+    const uint32_t layer = boards.layers[fullChanIndex] - 1;
+    const uint32_t detRegion = boards.detRegions[fullChanIndex] - 1;
+    const uint32_t sector = boards.sectors[fullChanIndex] - 1;
+
+    // Calculate the index to get the geometry of the board
+    const uint32_t idx = station * UT::Decoding::ut_number_of_sectors_per_board + layer * 3 + detRegion;
+    const uint32_t idx_offset = dev_ut_region_offsets[idx] + sector;
+
+    uint* hits_sector_group = hit_offsets + dev_unique_x_sector_offsets[idx_offset];
+    atomicAdd(hits_sector_group, 1);
+  }
+
+}
+
 /**
  * @brief Calculates the number of hits to be decoded for the UT detector.
  */
@@ -26,23 +54,38 @@ __global__ void ut_calculate_number_of_hits(
   for (uint32_t raw_bank_index = threadIdx.x; raw_bank_index < raw_event.number_of_raw_banks;
        raw_bank_index += blockDim.x) {
     const UTRawBank raw_bank = raw_event.getUTRawBank(raw_bank_index);
-    const uint32_t m_nStripsPerHybrid = boards.stripsPerHybrids[raw_bank.sourceID];
+    calculate_number_of_hits(dev_ut_region_offsets, dev_unique_x_sector_offsets, hit_offsets, boards, raw_bank);
+  }
+}
 
-    for (uint32_t i = threadIdx.y; i < raw_bank.number_of_hits; i += blockDim.y) {
-      const uint32_t channelID = (raw_bank.data[i] & UT::Decoding::chan_mask) >> UT::Decoding::chan_offset;
-      const uint32_t index = channelID / m_nStripsPerHybrid;
-      const uint32_t fullChanIndex = raw_bank.sourceID * UT::Decoding::ut_number_of_sectors_per_board + index;
-      const uint32_t station = boards.stations[fullChanIndex] - 1;
-      const uint32_t layer = boards.layers[fullChanIndex] - 1;
-      const uint32_t detRegion = boards.detRegions[fullChanIndex] - 1;
-      const uint32_t sector = boards.sectors[fullChanIndex] - 1;
+/**
+ * @brief Calculates the number of hits to be decoded for the UT detector.
+ */
+__global__ void ut_calculate_number_of_hits_mep(
+  const char* dev_ut_raw_input,
+  const uint32_t* dev_ut_raw_input_offsets,
+  const char* ut_boards,
+  const uint* dev_ut_region_offsets,
+  const uint* dev_unique_x_sector_layer_offsets,
+  const uint* dev_unique_x_sector_offsets,
+  uint32_t* dev_ut_hit_offsets,
+  const uint* dev_event_list)
+{
+  const uint32_t event_number = blockIdx.x;
+  const uint selected_event_number = dev_event_list[event_number];
 
-      // Calculate the index to get the geometry of the board
-      const uint32_t idx = station * UT::Decoding::ut_number_of_sectors_per_board + layer * 3 + detRegion;
-      const uint32_t idx_offset = dev_ut_region_offsets[idx] + sector;
+  const uint number_of_unique_x_sectors = dev_unique_x_sector_layer_offsets[4];
+  uint32_t* hit_offsets = dev_ut_hit_offsets + event_number * number_of_unique_x_sectors;
 
-      uint* hits_sector_group = hit_offsets + dev_unique_x_sector_offsets[idx_offset];
-      atomicAdd(hits_sector_group, 1);
-    }
+  const UTBoards boards(ut_boards);
+  auto const number_of_ut_raw_banks = dev_ut_raw_input_offsets[0];
+
+  for (uint32_t raw_bank_index = threadIdx.x; raw_bank_index < number_of_ut_raw_banks;
+       raw_bank_index += blockDim.x) {
+
+    auto const source_id = dev_ut_raw_input_offsets[2 + raw_bank_index];
+    auto const fragment_offset = dev_ut_raw_input_offsets[2 + number_of_ut_raw_banks * (1 + selected_event_number) + raw_bank_index];
+    UTRawBank const raw_bank{source_id, dev_ut_raw_input + fragment_offset};
+    calculate_number_of_hits(dev_ut_region_offsets, dev_unique_x_sector_offsets, hit_offsets, boards, raw_bank);
   }
 }
