4254993524ac026312c70f1ea979fa907c7859a0
Daniel Campora <dcampora@cern.ch>
2019-10-19 19:13:43 +0200

    Fixed a minor bug.

--- cuda/SciFi/looking_forward/triplet_seeding/src/LFTripletSeedingImpl.cu
+++ cuda/SciFi/looking_forward/triplet_seeding/src/LFTripletSeedingImpl.cu
@@ -61,90 +61,21 @@ __device__ void lf_triplet_seeding_impl(
         chi2 = extrap1 + chi2 * chi2;
 
         if (chi2 < LookingForward::chi2_max_triplet_single) {
-          best_combined.push_back(CombinedTripletValue{chi2,
-                                  (int16_t) i,
-                                  (int16_t) k,
-                                  (int16_t) j});
+          best_combined.push_back(CombinedTripletValue {chi2, (int16_t) i, (int16_t) k, (int16_t) j});
         }
       }
     }
   }
 
-  // // Search best triplets per h1
-  // // Tiled processing of h0 and h2
-  // for (int i = 0; i < (l0_size + LookingForward::tile_size - 1) / LookingForward::tile_size; ++i) {
-  //   for (int j = 0; j < (l2_size + LookingForward::tile_size - 1) / LookingForward::tile_size; ++j) {
-  //     __syncthreads();
+  std::sort(
+    best_combined.begin(), best_combined.end(), [](const CombinedTripletValue& a, const CombinedTripletValue& b) {
+      return a.chi2 < b.chi2;
+    });
 
-  //     // Search best triplets per h1
-  //     for (uint k = threadIdx.x; k < LookingForward::tile_size * LookingForward::tile_size; k += blockDim.x) {
-  //       const uint h0_rel = i * LookingForward::tile_size + (k % LookingForward::tile_size);
-  //       const uint h2_rel = j * LookingForward::tile_size + (k / LookingForward::tile_size);
-
-  //       auto partial_chi2 = 10000.f * LookingForward::chi2_max_triplet_single;
-  //       if (h0_rel < l0_size && h2_rel < l2_size) {
-  //         const auto x0 = scifi_hits_x0[l0_start + h0_rel];
-  //         const auto x2 = scifi_hits_x0[l2_start + h2_rel];
-  //         partial_chi2 = x2 - x0 + x0 * zdiff - extrap2;
-  //       }
-  //       shared_partial_chi2[k] = partial_chi2;
-  //     }
-
-  //     __syncthreads();
-
-  //     // Iterate over all h1s
-  //     // Find best chi2, h0 and h2 using the partial chi2 from before
-  //     for (uint h1_rel = threadIdx.x; h1_rel < l1_size; h1_rel += blockDim.x) {
-  //       const float x1_zdiff = scifi_hits_x0[l1_start + h1_rel] * zdiff;
-
-  //       float best_chi2 = LookingForward::chi2_max_triplet_single;
-  //       int best_k = -1;
-
-  //       for (int k = 0; k < LookingForward::tile_size * LookingForward::tile_size; ++k) {
-  //         float chi2 = shared_partial_chi2[k] - x1_zdiff;
-  //         chi2 = extrap1 + chi2 * chi2;
-
-  //         if (chi2 < best_chi2) {
-  //           best_chi2 = chi2;
-  //           best_k = k;
-  //         }
-  //       }
-
-  //       // if (best_k != -1 && best_chi2 < best_combined[h1_rel].chi2) {
-  //       //   best_combined[h1_rel] =
-  //       //     SciFi::CombinedValue {best_chi2,
-  //       //                           (int16_t)(i * LookingForward::tile_size + (best_k % LookingForward::tile_size)),
-  //       //                           (int16_t)(j * LookingForward::tile_size + (best_k / LookingForward::tile_size))};
-  //       // }
-
-  //       if (best_k != -1) {
-  //         best_combined.push_back(CombinedTripletValue{best_chi2,
-  //                                 (int16_t)(i * LookingForward::tile_size + (best_k % LookingForward::tile_size)),
-  //                                 (int16_t) h1_rel,
-  //                                 (int16_t)(j * LookingForward::tile_size + (best_k / LookingForward::tile_size))});
-  //       }
-  //     }
-  //   }
-  // }
-
-  // for (uint i = 0; i < l1_size; ++i) {
-  //   const auto best_combo = best_combined[i];
-  //   printf(" %i, %i, %f\n", best_combo.h0, best_combo.h2, best_combo.chi2);
-  // }
-
-  // std::sort(best_combined_keys.begin(), best_combined_keys.end(), [&](const int a, const int b) {
-  //   return best_combined[a].chi2 < best_combined[b].chi2;
-  // });
-
-  std::sort(best_combined.begin(), best_combined.end(), [] (const CombinedTripletValue& a, const CombinedTripletValue& b) {
-    return a.chi2 < b.chi2;
-  });
-
-  // Note: LookingForward::maximum_number_of_candidates_per_ut_track / 2 due to (0, 2, 4) and (1, 3, 5) being looked
-  //       at separately.
-  for (int i = 0; i < (LookingForward::maximum_number_of_candidates_per_ut_track / number_of_seeds) && i < l1_size;
+  // Note: LookingForward::maximum_number_of_candidates_per_ut_track / number of seeds is the maximum that can be stored
+  for (int i = 0;
+       i < (LookingForward::maximum_number_of_candidates_per_ut_track / number_of_seeds) && i < best_combined.size();
        ++i) {
-    // const auto best_h1 = best_combined_keys[i];
     const auto best_combo = best_combined[i];
 
     if (best_combo.h0 != -1) {
