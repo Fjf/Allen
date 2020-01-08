b8677a7ec7b0641aadc9f4bf8ae05f47ac0cc152
Daniel Campora <dcampora@cern.ch>
2020-01-08 10:37:19 +0100

    Fixed sbt.

--- cuda/velo/search_by_triplet/src/SearchByTriplet.cu
+++ cuda/velo/search_by_triplet/src/SearchByTriplet.cu
@@ -43,7 +43,8 @@ __constant__ int Configuration::velo_search_by_triplet::ttf_modulo_mask;
 
 __global__ void velo_search_by_triplet::velo_search_by_triplet(
   velo_search_by_triplet::Parameters parameters,
-  const VeloGeometry* dev_velo_geometry) {
+  const VeloGeometry* dev_velo_geometry)
+{
   /* Data initialization */
   // Each event is treated with two blocks, one for each side.
   const uint event_number = blockIdx.x;
@@ -52,30 +53,33 @@ __global__ void velo_search_by_triplet::velo_search_by_triplet(
 
   // Pointers to data within the event
   const uint total_estimated_number_of_clusters =
-    arguments.dev_offsets_estimated_input_size[Velo::Constants::n_modules * number_of_events];
-  const uint* module_hitStarts = arguments.dev_offsets_estimated_input_size + event_number * Velo::Constants::n_modules;
-  const uint* module_hitNums = arguments.dev_module_cluster_num + event_number * Velo::Constants::n_modules;
+    parameters.dev_offsets_estimated_input_size[Velo::Constants::n_modules * number_of_events];
+  const uint* module_hitStarts =
+    parameters.dev_offsets_estimated_input_size + event_number * Velo::Constants::n_modules;
+  const uint* module_hitNums = parameters.dev_module_cluster_num + event_number * Velo::Constants::n_modules;
   const uint hit_offset = module_hitStarts[0];
 
   // Think whether this offset'ed container is a good solution
-  const auto velo_cluster_container =
-    Velo::Clusters<const uint>{arguments.dev_sorted_velo_cluster_container.get() + hit_offset, total_estimated_number_of_clusters};
+  const auto velo_cluster_container = Velo::Clusters<const uint> {
+    parameters.dev_sorted_velo_cluster_container.get() + hit_offset, total_estimated_number_of_clusters};
 
-  const auto hit_phi = arguments.dev_hit_phi + hit_offset;
+  const auto hit_phi = parameters.dev_hit_phi + hit_offset;
 
   // Per event datatypes
-  Velo::TrackHits* tracks = arguments.dev_tracks + tracks_offset;
+  Velo::TrackHits* tracks = parameters.dev_tracks + tracks_offset;
 
   // Per side datatypes
-  bool* hit_used = arguments.dev_hit_used + hit_offset;
-  const short* h0_candidates = arguments.dev_h0_candidates + 2 * hit_offset;
-  const short* h2_candidates = arguments.dev_h2_candidates + 2 * hit_offset;
+  bool* hit_used = parameters.dev_hit_used + hit_offset;
+  const short* h0_candidates = parameters.dev_h0_candidates + 2 * hit_offset;
+  const short* h2_candidates = parameters.dev_h2_candidates + 2 * hit_offset;
 
-  uint* tracks_to_follow = arguments.dev_tracks_to_follow + event_number * Configuration::velo_search_by_triplet::ttf_modulo;
+  uint* tracks_to_follow =
+    parameters.dev_tracks_to_follow + event_number * Configuration::velo_search_by_triplet::ttf_modulo;
   Velo::TrackletHits* three_hit_tracks =
-    arguments.dev_three_hit_tracks + event_number * Configuration::velo_search_by_triplet::max_weak_tracks;
-  Velo::TrackletHits* tracklets = arguments.dev_tracklets + event_number * Configuration::velo_search_by_triplet::ttf_modulo;
-  unsigned short* h1_rel_indices = arguments.dev_rel_indices + event_number * Velo::Constants::max_numhits_in_module;
+    parameters.dev_three_hit_tracks + event_number * Configuration::velo_search_by_triplet::max_weak_tracks;
+  Velo::TrackletHits* tracklets =
+    parameters.dev_tracklets + event_number * Configuration::velo_search_by_triplet::ttf_modulo;
+  unsigned short* h1_rel_indices = parameters.dev_rel_indices + event_number * Velo::Constants::max_numhits_in_module;
 
   // Shared memory size is defined externally
   __shared__ float module_data[12];
@@ -97,6 +101,6 @@ __global__ void velo_search_by_triplet::velo_search_by_triplet(
     h1_rel_indices,
     hit_offset,
     dev_velo_geometry->module_zs,
-    arguments.dev_atomics_velo + blockIdx.x * Velo::num_atomics,
-    arguments.dev_number_of_velo_tracks);
+    parameters.dev_atomics_velo + blockIdx.x * Velo::num_atomics,
+    parameters.dev_number_of_velo_tracks);
 }
