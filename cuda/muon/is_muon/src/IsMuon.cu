d01d171644a701ea233d5e7768da33023ebc09be
Florian <freiss@cern.ch>
2020-01-28 19:43:13 +0100

    extrapolate to muon z hit positions

--- cuda/muon/is_muon/src/IsMuon.cu
+++ cuda/muon/is_muon/src/IsMuon.cu
@@ -80,13 +80,14 @@ __global__ void is_muon::is_muon(
       const float station_z = muon_hits_event.z[station_offset];
       const auto& state = scifi_tracks.states(track_id);
 
-      const float extrapolation_x = state.x + state.tx * (station_z - state.z);
-      const float extrapolation_y = state.y + state.ty * (station_z - state.z);
-
       parameters.dev_muon_track_occupancies[track_offset + station_id] = 0;
 
       for (int i_hit = 0; i_hit < number_of_hits; ++i_hit) {
         const int idx = station_offset + i_hit;
+        const float extrapolation_x = scifi_tracks.states[track_id].x +
+                                    scifi_tracks.states[track_id].tx * (muon_hits_event.z[idx] - scifi_tracks.states[track_id].z);
+        const float extrapolation_y = scifi_tracks.states[track_id].y +
+                                    scifi_tracks.states[track_id].ty * (muon_hits_event.z[idx]- scifi_tracks.states[track_id].z);
         if (is_in_window(
               muon_hits_event.x[idx],
               muon_hits_event.y[idx],
