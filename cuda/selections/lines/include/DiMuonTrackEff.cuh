fbb00ff694fe70a25f8d6879958fde1e31bfbb25
Adrián Casais <adrian.casais@rai.usc.es>
2020-01-31 14:22:50 +0100

    different cuts

--- cuda/selections/lines/include/DiMuonTrackEff.cuh
+++ cuda/selections/lines/include/DiMuonTrackEff.cuh
@@ -6,16 +6,16 @@
 
 namespace DiMuonTrackEff {
   // Mass window around J/psi meson.
-  constexpr float DMTrackEffM0 = 2900.f;
-  constexpr float DMTrackEffM1 = 3100.f;
+  constexpr float DMTrackEffM0 = 2000.f;
+  constexpr float DMTrackEffM1 = 4000.f;
 
   struct DiMuonTrackEff_t : public Hlt1::VeloUTTwoTrackLine {
     constexpr static auto name {"DiMuonTrackEff"};
 
     static __device__ bool function(const VertexFit::TrackMVAVertex& vertex)
     {
-      if (!vertex.is_dimuon) return false;
-      const bool decision = vertex.chi2 > 0 && vertex.mdimu > DMTrackEffM0 && vertex.mdimu < DMTrackEffM1;
+      //if (!vertex.is_dimuon) return false;
+      const bool decision = vertex.mdimu > DMTrackEffM0 && vertex.mdimu < DMTrackEffM1;
       return decision;
     }
   };
