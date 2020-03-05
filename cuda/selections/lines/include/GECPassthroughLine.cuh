5b6b1f46cb1a47e141cb8691b556b575c09cead8
Thomas Julian Boettcher <tboettch@mit.edu>
2020-03-05 17:01:16 +0000

    Create GEC-only passthrough line

---
+++ cuda/selections/lines/include/GECPassthroughLine.cuh
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "LineInfo.cuh"
+#include "Event/ODIN.h"
+
+namespace GECPassthrough {
+
+  // Line for checking if an event passes the GEC.
+  struct GECPassthrough_t : public Hlt1::VeloLine {
+    constexpr static auto name {"GECPassthrough"};
+    static __device__ bool function(const uint) {
+      return true;
+    }
+  };
+  
+} // namespace GECPassthrough
