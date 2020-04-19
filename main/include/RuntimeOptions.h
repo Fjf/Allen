bce5865d057de5144c2e823f716ae977b9e82b18
Roel Aaij <roel.aaij@cern.ch>
2020-04-19 22:33:15 +0200

    Remove explicit banks from RuntimeOptions

--- main/include/RuntimeOptions.h
+++ main/include/RuntimeOptions.h
@@ -10,10 +10,6 @@
 struct RuntimeOptions {
   IInputProvider const* input_provider;
   size_t const slice_index;
-  BanksAndOffsets host_ut_events;
-  BanksAndOffsets host_scifi_events;
-  BanksAndOffsets host_muon_events;
-  BanksAndOffsets host_odin_events;
   std::tuple<uint, uint> event_interval;
   uint number_of_selected_events;
   uint number_of_repetitions;
@@ -26,18 +22,12 @@ struct RuntimeOptions {
   RuntimeOptions(
     IInputProvider const* ip,
     size_t const index,
-    BanksAndOffsets ut_events,
-    BanksAndOffsets scifi_events,
-    BanksAndOffsets muon_events,
-    BanksAndOffsets odin_events,
     std::tuple<uint, uint> param_event_interval,
     uint param_number_of_repetitions,
     bool param_do_check,
     bool param_cpu_offload,
     bool param_mep_layout) :
     input_provider{ip}, slice_index{index},
-    host_ut_events {std::move(ut_events)}, host_scifi_events {std::move(scifi_events)},
-    host_muon_events {std::move(muon_events)}, host_odin_events {std::move(odin_events)},
     event_interval(param_event_interval),
     number_of_selected_events(std::get<1>(param_event_interval) - std::get<0>(param_event_interval)),
     number_of_repetitions(param_number_of_repetitions), do_check(param_do_check),
