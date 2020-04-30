65bd52883adeebc473bacc1848476bbe406fb886
Roel Aaij <roel.aaij@cern.ch>
2020-04-30 13:42:13 +0200

    Use codecs to deal with utf-8 encoded files

--- scripts/ParseAlgorithms.py
+++ scripts/ParseAlgorithms.py
@@ -3,6 +3,7 @@
 import re
 import os
 import sys
+import codecs
 from collections import OrderedDict
 from AlgorithmTraversalLibTooling import AlgorithmTraversal
 from LineTraversalLibTooling import LineTraversal
@@ -66,25 +67,24 @@ class Parser():
         algorithms = []
         lines = []
         for filename in all_filenames:
-            f = open(filename)
-            try:
-                s = f.read()
-                f.close()
-                # Invoke the libTooling algorithm parser only if we find the algorithm pattern
-                has_algorithm = Parser.__algorithm_pattern_compiled.search(s)
-                if has_algorithm:
-                    parsed_algorithms = algorithm_parser.traverse(filename)
-                    if parsed_algorithms:
-                        algorithms += parsed_algorithms
-                # Invoke the libTooling line parser only if we find the line pattern
-                has_line = Parser.__line_pattern_compiled.search(s)
-                if has_line:
-                    parsed_lines = line_parser.traverse(filename)
-                    if parsed_lines:
-                        lines += parsed_lines
-            except:
-                print("Parsing file", filename, "failed")
-                raise
+            with codecs.open(filename, 'r', 'utf-8') as f:
+                try:
+                    s = f.read()
+                    # Invoke the libTooling algorithm parser only if we find the algorithm pattern
+                    has_algorithm = Parser.__algorithm_pattern_compiled.search(s)
+                    if has_algorithm:
+                        parsed_algorithms = algorithm_parser.traverse(filename)
+                        if parsed_algorithms:
+                            algorithms += parsed_algorithms
+                    # Invoke the libTooling line parser only if we find the line pattern
+                    has_line = Parser.__line_pattern_compiled.search(s)
+                    if has_line:
+                        parsed_lines = line_parser.traverse(filename)
+                        if parsed_lines:
+                            lines += parsed_lines
+                except:
+                    print("Parsing file", filename, "failed")
+                    raise
         return algorithms, lines
 
 
