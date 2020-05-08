5cb23e2e73375e8a6bc91ec7d74bd1d754c1d450
Dorothea vom Bruch <Dorothea vom Bruch dorothea.vom.bruch@cern.ch>
2020-05-08 11:25:19 +0200

    test gaud_env variable

--- Rec/Allen/src/RunAllen.h
+++ Rec/Allen/src/RunAllen.h
@@ -77,8 +77,8 @@ private:
 
   Gaudi::Property<std::string> m_updaterName {this, "UpdaterName", "AllenUpdater"};
 
-  // to do: don't use hard-coded path
-  Gaudi::Property<std::string> m_json {this, "JSON", "${ALLEN_PROJECT_ROOT}/build.x86_64-centos7-gcc9-opt/Sequence.json"};
+  //Gaudi::Property<std::string> m_json {this, "JSON", "${ALLEN_INSTALL_DIR}/constants/Sequence.json"}; 
+  Gaudi::Property<std::string> m_json {this, "JSON", "${ALLEN_PROJECT_ROOT}/build.x86_64-centos7-gcc9-opt/Sequence.json"}; 
   Gaudi::Property<std::string> m_paramDir {this, "ParamDir", "${ALLEN_PROJECT_ROOT}/input/detector_configuration/down"};
 
   // If set to false, events are only filtered by the GEC
