c83cc7cbf8b3c109a3fce9f82f6d63d4acc76817
Daniel Campora <dcampora@cern.ch>
2020-05-18 17:04:51 +0200

    Pregenerated all sequences.

--- scripts/pregenerate_sequences.sh
+++ scripts/pregenerate_sequences.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-SCRIPTS_DIR="${PWD}/../scripts"
-SEQUENCE_DIR="${PWD}/../configuration/sequences"
-PREGENERATED_DIR="${PWD}/../configuration/pregenerated"
+BASE_DIR="${PWD}/.."
+SCRIPTS_DIR="${BASE_DIR}/scripts"
+SEQUENCE_DIR="${BASE_DIR}/configuration/sequences"
+PREGENERATED_DIR="${BASE_DIR}/configuration/pregenerated"
 
-LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/releases/clang/10.0.0-62e61/x86_64-centos7/lib:/cvmfs/sft.cern.ch/lcg/releases/gcc/9.2.0-afc57/x86_64-centos7/lib:/cvmfs/sft.cern.ch/lcg/releases/gcc/9.2.0-afc57/x86_64-centos7/lib64:${LD_LIBRARY_PATH} python3 ${SCRIPTS_DIR}/ParseAlgorithms.py
+LD_LIBRARY_PATH=/cvmfs/sft.cern.ch/lcg/releases/clang/10.0.0-62e61/x86_64-centos7/lib:/cvmfs/sft.cern.ch/lcg/releases/gcc/9.2.0-afc57/x86_64-centos7/lib64:${LD_LIBRARY_PATH} CPLUS_INCLUDE_PATH=/cvmfs/sft.cern.ch/lcg/views/LCG_97python3/x86_64-centos7-gcc8-opt/include:${CPLUS_INCLUDE_PATH} python3 ${SCRIPTS_DIR}/ParseAlgorithms.py ${SEQUENCE_DIR}/definitions/algorithms.py ${BASE_DIR}/ Allen
 
-mv algorithms.py ${SEQUENCE_DIR}/definitions/
 cd ${SEQUENCE_DIR}
 
 for sequence in `ls | egrep ".py$" --color=none`; do
