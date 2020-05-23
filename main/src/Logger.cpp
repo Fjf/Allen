626366e82fc05b1d178352f36cded946724af371
Daniel Campora <dcampora@cern.ch>
2020-05-23 08:30:39 +0200

    Fix compilation issues with boost iostreams.

--- main/src/Logger.cpp
+++ main/src/Logger.cpp
@@ -9,6 +9,7 @@
 
 namespace logger {
   static std::unique_ptr<Logger> ll;
+  static std::unique_ptr<boost::iostreams::stream<boost::iostreams::null_sink>> nullOstream;
 }
 
 void logger::setVerbosity(int level)
@@ -32,10 +33,13 @@ std::ostream& logger::logger(int requestedLogLevel)
   if (!logger::ll) {
     logger::ll.reset(new logger::Logger {});
   }
+  if (!logger::nullOstream) {
+    logger::nullOstream.reset(new boost::iostreams::stream<boost::iostreams::null_sink>{boost::iostreams::null_sink()});
+  }
   if (logger::ll->verbosityLevel >= requestedLogLevel) {
     return std::cout;
   }
   else {
-    return boost::iostreams::stream<boost::iostreams::null_sink> nullOstream {boost::iostreams::null_sink()};
+    return *logger::nullOstream;
   }
 }
