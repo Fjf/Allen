1844d68da1888e081ab4ad83119119c5c909abeb
Roel Aaij <roel.aaij@cern.ch>
2020-06-24 09:33:57 +0200

    Update all sequences

--- configuration/pregenerated/hlt1_pp_scifi_v6_input_aggregates.h
+++ configuration/pregenerated/hlt1_pp_scifi_v6_input_aggregates.h
@@ -1,148 +1,25 @@
 #pragma once
 
 #include <tuple>
-#include "../../device/selections/lines/include/BeamCrossingLine.cuh"
-#include "../../device/selections/lines/include/TrackMVALine.cuh"
 #include "../../device/selections/lines/include/TwoTrackMVALine.cuh"
+#include "../../device/selections/lines/include/BeamCrossingLine.cuh"
+#include "../../device/selections/lines/include/VeloMicroBiasLine.cuh"
+#include "../../device/selections/lines/include/TrackMVALine.cuh"
 
-struct track_mva_line__dev_decisions_t : track_mva_line::Parameters::dev_decisions_t {
-  using type = track_mva_line::Parameters::dev_decisions_t::type;
-  void set_size(size_t size) override { m_size = size; }
-  size_t size() const override { return m_size; }
-  std::string name() const override { return "track_mva_line__dev_decisions_t"; }
-  void set_offset(char* offset) override { m_offset = offset; }
-  char* offset() const override { return m_offset; }
+struct track_mva_line__dev_decisions_t : track_mva_line::Parameters::dev_decisions_t { using type = track_mva_line::Parameters::dev_decisions_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "track_mva_line__dev_decisions_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct two_track_mva_line__dev_decisions_t : two_track_mva_line::Parameters::dev_decisions_t { using type = two_track_mva_line::Parameters::dev_decisions_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "two_track_mva_line__dev_decisions_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct no_beam_line__dev_decisions_t : beam_crossing_line::Parameters::dev_decisions_t { using type = beam_crossing_line::Parameters::dev_decisions_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "no_beam_line__dev_decisions_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct beam_one_line__dev_decisions_t : beam_crossing_line::Parameters::dev_decisions_t { using type = beam_crossing_line::Parameters::dev_decisions_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "beam_one_line__dev_decisions_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct beam_two_line__dev_decisions_t : beam_crossing_line::Parameters::dev_decisions_t { using type = beam_crossing_line::Parameters::dev_decisions_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "beam_two_line__dev_decisions_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct both_beams_line__dev_decisions_t : beam_crossing_line::Parameters::dev_decisions_t { using type = beam_crossing_line::Parameters::dev_decisions_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "both_beams_line__dev_decisions_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct velo_micro_bias_line__dev_decisions_t : velo_micro_bias_line::Parameters::dev_decisions_t { using type = velo_micro_bias_line::Parameters::dev_decisions_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "velo_micro_bias_line__dev_decisions_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct track_mva_line__dev_decisions_offsets_t : track_mva_line::Parameters::dev_decisions_offsets_t { using type = track_mva_line::Parameters::dev_decisions_offsets_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "track_mva_line__dev_decisions_offsets_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct two_track_mva_line__dev_decisions_offsets_t : two_track_mva_line::Parameters::dev_decisions_offsets_t { using type = two_track_mva_line::Parameters::dev_decisions_offsets_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "two_track_mva_line__dev_decisions_offsets_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct no_beam_line__dev_decisions_offsets_t : beam_crossing_line::Parameters::dev_decisions_offsets_t { using type = beam_crossing_line::Parameters::dev_decisions_offsets_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "no_beam_line__dev_decisions_offsets_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct beam_one_line__dev_decisions_offsets_t : beam_crossing_line::Parameters::dev_decisions_offsets_t { using type = beam_crossing_line::Parameters::dev_decisions_offsets_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "beam_one_line__dev_decisions_offsets_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct beam_two_line__dev_decisions_offsets_t : beam_crossing_line::Parameters::dev_decisions_offsets_t { using type = beam_crossing_line::Parameters::dev_decisions_offsets_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "beam_two_line__dev_decisions_offsets_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct both_beams_line__dev_decisions_offsets_t : beam_crossing_line::Parameters::dev_decisions_offsets_t { using type = beam_crossing_line::Parameters::dev_decisions_offsets_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "both_beams_line__dev_decisions_offsets_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
+struct velo_micro_bias_line__dev_decisions_offsets_t : velo_micro_bias_line::Parameters::dev_decisions_offsets_t { using type = velo_micro_bias_line::Parameters::dev_decisions_offsets_t::type; void set_size(size_t size) override { m_size = size; } size_t size() const override { return m_size; } std::string name() const override { return "velo_micro_bias_line__dev_decisions_offsets_t"; } void set_offset(char* offset) override { m_offset = offset; } char* offset() const override { return m_offset; } private: size_t m_size = 0; char* m_offset = nullptr; };
 
-private:
-  size_t m_size = 0;
-  char* m_offset = nullptr;
-};
-struct two_track_mva_line__dev_decisions_t : two_track_mva_line::Parameters::dev_decisions_t {
-  using type = two_track_mva_line::Parameters::dev_decisions_t::type;
-  void set_size(size_t size) override { m_size = size; }
-  size_t size() const override { return m_size; }
-  std::string name() const override { return "two_track_mva_line__dev_decisions_t"; }
-  void set_offset(char* offset) override { m_offset = offset; }
-  char* offset() const override { return m_offset; }
-
-private:
-  size_t m_size = 0;
-  char* m_offset = nullptr;
-};
-struct no_beam_line__dev_decisions_t : beam_crossing_line::Parameters::dev_decisions_t {
-  using type = beam_crossing_line::Parameters::dev_decisions_t::type;
-  void set_size(size_t size) override { m_size = size; }
-  size_t size() const override { return m_size; }
-  std::string name() const override { return "no_beam_line__dev_decisions_t"; }
-  void set_offset(char* offset) override { m_offset = offset; }
-  char* offset() const override { return m_offset; }
-
-private:
-  size_t m_size = 0;
-  char* m_offset = nullptr;
-};
-struct beam_one_line__dev_decisions_t : beam_crossing_line::Parameters::dev_decisions_t {
-  using type = beam_crossing_line::Parameters::dev_decisions_t::type;
-  void set_size(size_t size) override { m_size = size; }
-  size_t size() const override { return m_size; }
-  std::string name() const override { return "beam_one_line__dev_decisions_t"; }
-  void set_offset(char* offset) override { m_offset = offset; }
-  char* offset() const override { return m_offset; }
-
-private:
-  size_t m_size = 0;
-  char* m_offset = nullptr;
-};
-struct beam_two_line__dev_decisions_t : beam_crossing_line::Parameters::dev_decisions_t {
-  using type = beam_crossing_line::Parameters::dev_decisions_t::type;
-  void set_size(size_t size) override { m_size = size; }
-  size_t size() const override { return m_size; }
-  std::string name() const override { return "beam_two_line__dev_decisions_t"; }
-  void set_offset(char* offset) override { m_offset = offset; }
-  char* offset() const override { return m_offset; }
-
-private:
-  size_t m_size = 0;
-  char* m_offset = nullptr;
-};
-struct track_mva_line__dev_decisions_offsets_t : track_mva_line::Parameters::dev_decisions_offsets_t {
-  using type = track_mva_line::Parameters::dev_decisions_offsets_t::type;
-  void set_size(size_t size) override { m_size = size; }
-  size_t size() const override { return m_size; }
-  std::string name() const override { return "track_mva_line__dev_decisions_offsets_t"; }
-  void set_offset(char* offset) override { m_offset = offset; }
-  char* offset() const override { return m_offset; }
-
-private:
-  size_t m_size = 0;
-  char* m_offset = nullptr;
-};
-struct two_track_mva_line__dev_decisions_offsets_t : two_track_mva_line::Parameters::dev_decisions_offsets_t {
-  using type = two_track_mva_line::Parameters::dev_decisions_offsets_t::type;
-  void set_size(size_t size) override { m_size = size; }
-  size_t size() const override { return m_size; }
-  std::string name() const override { return "two_track_mva_line__dev_decisions_offsets_t"; }
-  void set_offset(char* offset) override { m_offset = offset; }
-  char* offset() const override { return m_offset; }
-
-private:
-  size_t m_size = 0;
-  char* m_offset = nullptr;
-};
-struct no_beam_line__dev_decisions_offsets_t : beam_crossing_line::Parameters::dev_decisions_offsets_t {
-  using type = beam_crossing_line::Parameters::dev_decisions_offsets_t::type;
-  void set_size(size_t size) override { m_size = size; }
-  size_t size() const override { return m_size; }
-  std::string name() const override { return "no_beam_line__dev_decisions_offsets_t"; }
-  void set_offset(char* offset) override { m_offset = offset; }
-  char* offset() const override { return m_offset; }
-
-private:
-  size_t m_size = 0;
-  char* m_offset = nullptr;
-};
-struct beam_one_line__dev_decisions_offsets_t : beam_crossing_line::Parameters::dev_decisions_offsets_t {
-  using type = beam_crossing_line::Parameters::dev_decisions_offsets_t::type;
-  void set_size(size_t size) override { m_size = size; }
-  size_t size() const override { return m_size; }
-  std::string name() const override { return "beam_one_line__dev_decisions_offsets_t"; }
-  void set_offset(char* offset) override { m_offset = offset; }
-  char* offset() const override { return m_offset; }
-
-private:
-  size_t m_size = 0;
-  char* m_offset = nullptr;
-};
-struct beam_two_line__dev_decisions_offsets_t : beam_crossing_line::Parameters::dev_decisions_offsets_t {
-  using type = beam_crossing_line::Parameters::dev_decisions_offsets_t::type;
-  void set_size(size_t size) override { m_size = size; }
-  size_t size() const override { return m_size; }
-  std::string name() const override { return "beam_two_line__dev_decisions_offsets_t"; }
-  void set_offset(char* offset) override { m_offset = offset; }
-  char* offset() const override { return m_offset; }
-
-private:
-  size_t m_size = 0;
-  char* m_offset = nullptr;
-};
-
-namespace gather_selections {
-  namespace dev_input_selections_t {
-    using tuple_t = std::tuple<
-      track_mva_line__dev_decisions_t,
-      two_track_mva_line__dev_decisions_t,
-      no_beam_line__dev_decisions_t,
-      beam_one_line__dev_decisions_t,
-      beam_two_line__dev_decisions_t>;
-  }
-} // namespace gather_selections
-namespace gather_selections {
-  namespace dev_input_selections_offsets_t {
-    using tuple_t = std::tuple<
-      track_mva_line__dev_decisions_offsets_t,
-      two_track_mva_line__dev_decisions_offsets_t,
-      no_beam_line__dev_decisions_offsets_t,
-      beam_one_line__dev_decisions_offsets_t,
-      beam_two_line__dev_decisions_offsets_t>;
-  }
-} // namespace gather_selections
+namespace gather_selections { namespace dev_input_selections_t { using tuple_t = std::tuple<track_mva_line__dev_decisions_t, two_track_mva_line__dev_decisions_t, no_beam_line__dev_decisions_t, beam_one_line__dev_decisions_t, beam_two_line__dev_decisions_t, both_beams_line__dev_decisions_t, velo_micro_bias_line__dev_decisions_t>; }}
+namespace gather_selections { namespace dev_input_selections_offsets_t { using tuple_t = std::tuple<track_mva_line__dev_decisions_offsets_t, two_track_mva_line__dev_decisions_offsets_t, no_beam_line__dev_decisions_offsets_t, beam_one_line__dev_decisions_offsets_t, beam_two_line__dev_decisions_offsets_t, both_beams_line__dev_decisions_offsets_t, velo_micro_bias_line__dev_decisions_offsets_t>; }}
--- configuration/pregenerated/muon_input_aggregates.h
+++ configuration/pregenerated/muon_input_aggregates.h
@@ -2,13 +2,6 @@
 
 #include <tuple>
 
-namespace gather_selections {
-  namespace dev_input_selections_t {
-    using tuple_t = std::tuple<>;
-  }
-} // namespace gather_selections
-namespace gather_selections {
-  namespace dev_input_selections_offsets_t {
-    using tuple_t = std::tuple<>;
-  }
-} // namespace gather_selections
+
+namespace gather_selections { namespace dev_input_selections_t { using tuple_t = std::tuple<>; }}
+namespace gather_selections { namespace dev_input_selections_offsets_t { using tuple_t = std::tuple<>; }}
