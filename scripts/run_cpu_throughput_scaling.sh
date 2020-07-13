112a96e3f07b0ce9830e97215073f8e1e5f0ca6c
Daniel Campora <dcampora@cern.ch>
2020-07-13 17:54:24 +0200

    Updated script.

--- scripts/run_cpu_throughput_scaling.sh
+++ scripts/run_cpu_throughput_scaling.sh
@@ -17,7 +17,7 @@ NUM_NUMA_NODES=`lscpu | grep "NUMA node(s):" | awk '{ print $3 }'`
 ITERATIONS=$(($NUM_PROCESSORS / $NUM_NUMA_NODES))
 
 for i in `seq 1 ${ITERATIONS}`; do
-  if [ $i -le 3 ] || [ $(($i % 4)) -eq 0 ]; then
+  if [ $i -lt 3 ] || [ $(($i % 4)) -eq 0 ]; then
     echo "Running iteration ${i}/${ITERATIONS}..."
     numactl --cpunodebind=1 --membind=1 ${FOLDER}/Allen -f /scratch/dcampora/allen_data/201907/minbias_mag_down -n 200 -r 100 -c 0 -t $i > ${FOLDER}/${OUTPUT_FOLDER}/run_${i}.out
   fi
@@ -27,7 +27,7 @@ OUTPUT_FILE=${FOLDER}/${OUTPUT_FOLDER}/runs.csv
 touch $OUTPUT_FILE
 
 for i in `seq 1 ${ITERATIONS}`; do
-  if [ $i -le 3 ] || [ $(($i % 4)) -eq 0 ]; then
+  if [ $i -lt 3 ] || [ $(($i % 4)) -eq 0 ]; then
     value=`cat ${OUTPUT_FOLDER}/run_${i}.out | grep events/s | awk '{ print $1 }'`
     cores=$i
 
