e0a324852b7a6a8d5fab119352141aa85df1fe69
Maxime Schubiger <maxime.schubiger@nikhef.nl>
2020-08-20 17:06:07 +0200

    Save beamline and endvelo state to hostbuffer and fix the translation to V2 tracks

--- device/velo/simplified_kalman_filter/src/VeloKalmanFilter.cu
+++ device/velo/simplified_kalman_filter/src/VeloKalmanFilter.cu
@@ -30,7 +30,14 @@ void velo_kalman_filter::velo_kalman_filter_t::operator()(
 
   if (runtime_options.do_check) {
     cudaCheck(cudaMemcpyAsync(
-      host_buffers.host_kalmanvelo_states,
+      host_buffers.host_velo_kalman_beamline_states,
+      data<dev_velo_kalman_beamline_states_t>(arguments),
+      size<dev_velo_kalman_beamline_states_t>(arguments),
+      cudaMemcpyDeviceToHost,
+      cuda_stream));
+
+    cudaCheck(cudaMemcpyAsync(
+      host_buffers.host_velo_kalman_endvelo_states,
       data<dev_velo_kalman_endvelo_states_t>(arguments),
       size<dev_velo_kalman_endvelo_states_t>(arguments),
       cudaMemcpyDeviceToHost,
