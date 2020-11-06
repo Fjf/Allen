a83148765ea2d7d586189117bff12f8208a4d48c
Daniel Campora <dcampora@cern.ch>
2020-11-06 13:25:20 +0100

    Generate pregenerated sequences.

--- configuration/pregenerated/ut_input_aggregates.h
+++ configuration/pregenerated/ut_input_aggregates.h
@@ -1,34 +1,9 @@
-/*****************************************************************************\
-* (c) Copyright 2020 CERN for the benefit of the LHCb Collaboration           *
-*                                                                             *
-* This software is distributed under the terms of the GNU General Public      *
-* Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   *
-*                                                                             *
-* In applying this licence, CERN does not waive the privileges and immunities *
-* granted to it by virtue of its status as an Intergovernmental Organization  *
-* or submit itself to any jurisdiction.                                       *
-\*****************************************************************************/
 #pragma once
 
 #include <tuple>
 
-namespace gather_selections {
-  namespace dev_input_selections_t {
-    using tuple_t = std::tuple<>;
-  }
-} // namespace gather_selections
-namespace gather_selections {
-  namespace dev_input_selections_offsets_t {
-    using tuple_t = std::tuple<>;
-  }
-} // namespace gather_selections
-namespace gather_selections {
-  namespace host_input_post_scale_factors_t {
-    using tuple_t = std::tuple<>;
-  }
-} // namespace gather_selections
-namespace gather_selections {
-  namespace host_input_post_scale_hashes_t {
-    using tuple_t = std::tuple<>;
-  }
-} // namespace gather_selections
+
+namespace gather_selections { namespace dev_input_selections_t { using tuple_t = std::tuple<>; }}
+namespace gather_selections { namespace dev_input_selections_offsets_t { using tuple_t = std::tuple<>; }}
+namespace gather_selections { namespace host_input_post_scale_factors_t { using tuple_t = std::tuple<>; }}
+namespace gather_selections { namespace host_input_post_scale_hashes_t { using tuple_t = std::tuple<>; }}
