d67c492496995f5d02a9c43f82c66bab52de749f
Dorothea vom Bruch <Dorothea vom Bruch dorothea.vom.bruch@cern.ch>
2020-11-16 17:40:20 +0100

    read ecal and hcal clusters in AllenCaloToCaloClusters converter

--- Rec/Allen/src/AllenCaloToCaloClusters.cpp
+++ Rec/Allen/src/AllenCaloToCaloClusters.cpp
@@ -42,12 +42,31 @@ std::vector<LHCb::CaloCluster> AllenCaloToCaloClusters::operator()(const HostBuf
   //  const CaloCluster::CaloCluster allen_caloclusters
 
   unsigned number_of_ecal_clusters = host_buffers.host_ecal_cluster_offsets[number_of_events] - host_buffers.host_ecal_cluster_offsets[i_event]; 
-  unsigned number_of_hcal_clusters = host_buffers.host_hcal_cluster_offsets[number_of_events] - host_buffers.host_hcal_cluster_offsets[i_event]; 
+  unsigned number_of_hcal_clusters = host_buffers.host_hcal_cluster_offsets[number_of_events] - host_buffers.host_hcal_cluster_offsets[i_event];
+  CaloCluster* ecal_clusters = (CaloCluster*)(host_buffers.host_ecal_clusters.data());
+  CaloCluster* hcal_clusters = (CaloCluster*)(host_buffers.host_hcal_clusters.data());
 
   if (msgLevel(MSG::DEBUG)) {
     debug() << "Number of Ecal clusters to convert = " << number_of_ecal_clusters << endmsg;
     debug() << "Number of Hcal clusters to convert = " << number_of_hcal_clusters << endmsg;
   }
+
+  // Loop over Allen clusters and convert them
+  // Don't need to access them with offset since one event is processed at a time
+  for (unsigned i = 0; i < number_of_ecal_clusters; i++) {
+    const auto& cluster = ecal_clusters[i];
+    if (msgLevel(MSG::DEBUG)) {
+      debug() << " \t ECAL center_id = " << cluster.center_id << ", e = " << cluster.e << ", x = " << cluster.x << ", y = " << cluster.y << endmsg;
+    }
+  } 
+  
+  for (unsigned i = 0; i < number_of_hcal_clusters; i++) {
+    const auto& cluster = hcal_clusters[i];
+    if (msgLevel(MSG::DEBUG)) {
+      debug() << " \t HCAL center_id = " << cluster.center_id << ", e = " << cluster.e << ", x = " << cluster.x << ", y = " << cluster.y << endmsg;
+    }
+  } 
+
  
   return Clusters;
 }
