0e351968873e953466777396bf3e009a005b5560
Daniel Campora <dcampora@cern.ch>
2020-12-08 18:40:55 +0100

    Added Catch2 as a submodule, and added test folder with some tests (doesn't compile properly yet).

---
+++ test/unit_tests/generic/src/Catch2Main.cpp
@@ -0,0 +1,9 @@
+/*****************************************************************************\
+* (c) Copyright 2018-2020 CERN for the benefit of the LHCb Collaboration      *
+\*****************************************************************************/
+
+// It is generally recommended to have a single file provide the main
+// of a testing binary, and other test files to link against it.
+
+// Let Catch provide main():
+// #include <internal/catch_default_main.hpp>
