e74903fc499ff2f314608113339492f4a26f4de5
lbian <lingzhu.bian@cern.ch>
2020-12-15 18:29:54 +0800

    fix the bug of precision and modify the reference files

--- device/SciFi/consolidate/src/ConsolidateSciFi.cu
+++ device/SciFi/consolidate/src/ConsolidateSciFi.cu
@@ -51,7 +51,6 @@ __device__ void populate(const SciFi::TrackHits& track, const F& assign)
 
 __device__ float qop_calculation(
   LookingForward::Constants const* dev_looking_forward_constants,
-  SciFi::Consolidated::Tracks scifi_tracks,
   float const magSign,
   float const z0SciFi,
   float const x0SciFi,
@@ -71,31 +70,32 @@ __device__ float qop_calculation(
   const auto yVelo_at0 = yVelo - tyO * zVelo;
   const auto FLIGHTPATH_MAGNET_SCI_SQ =  (x0SciFi-xMatch)*(x0SciFi-xMatch) + (y0SciFi-yMatch)*(y0SciFi-yMatch)+ (z0SciFi-zMatch)*(z0SciFi-zMatch) ;
   const auto FLIGHTPATH_VELO_MAGNET_SQ =  (xVelo_at0-xMatch)*(xVelo_at0-xMatch) + (yVelo_at0-yMatch)*(yVelo_at0-yMatch)+ (0-zMatch)*(0-zMatch) ;
-  const float FLIGHTPATH = 0.001*sqrtf( FLIGHTPATH_MAGNET_SCI_SQ +FLIGHTPATH_VELO_MAGNET_SQ );
-  const float MAGFIELD =  FLIGHTPATH * cos(asin(tyO));
-  const float DSLOPE = txSciFi/( sqrtf( 1+ txSciFi * txSciFi + tySciFi * tySciFi)) - txO/(sqrtf( 1+ txO*txO + tyO*tyO));
+  const auto FLIGHTPATH = 0.001f*sqrtf( FLIGHTPATH_MAGNET_SCI_SQ +FLIGHTPATH_VELO_MAGNET_SQ );
+  const auto MAGFIELD =  FLIGHTPATH * cosf(asinf(tyO));
+  const auto DSLOPE = txSciFi/( sqrtf( 1+ txSciFi * txSciFi + tySciFi * tySciFi)) - txO/(sqrtf( 1+ txO*txO + tyO*tyO));
 
-  const float txO2 = pow(txO, 2);
-  const float txO3 = pow(txO, 3);
-  const float txO4 = pow(txO, 4);
-  const float txO5 = pow(txO, 5);
-  const float txO6 = pow(txO, 6);
-  const float txO7 = pow(txO, 7);   
-  const float tyO2 = pow(tyO, 2);
-  const float tyO4 = pow(tyO, 4);
-  const float tyO5 = pow(tyO, 5);
-  const float tyO6 = pow(tyO, 6);
+  const auto txO2 = powf(txO, 2);
+  const auto txO3 = powf(txO, 3);
+  const auto txO4 = powf(txO, 4);
+  const auto txO5 = powf(txO, 5);
+  const auto txO6 = powf(txO, 6);
+  const auto txO7 = powf(txO, 7);   
+  const auto tyO2 = powf(tyO, 2);
+  const auto tyO4 = powf(tyO, 4);
+  const auto tyO5 = powf(tyO, 5);
+  const auto tyO6 = powf(tyO, 6);
 
-  const float C0 = dev_looking_forward_constants->C0[0]+dev_looking_forward_constants->C0[1]*txO2 + dev_looking_forward_constants->C0[2]*txO4 + dev_looking_forward_constants->C0[3]*tyO2 + dev_looking_forward_constants->C0[4]*tyO4 + dev_looking_forward_constants->C0[5]*txO2*tyO2 + dev_looking_forward_constants->C0[6]*txO6 + dev_looking_forward_constants->C0[7]*tyO5 + dev_looking_forward_constants->C0[8]*txO4*tyO2 + dev_looking_forward_constants->C0[9]*txO2*tyO4;
-  const float C1 = dev_looking_forward_constants->C1[0]+dev_looking_forward_constants->C1[1]*txO + dev_looking_forward_constants->C1[2]*txO3 +dev_looking_forward_constants->C1[3]*txO5 +dev_looking_forward_constants->C1[4]*txO7 + dev_looking_forward_constants->C1[5]*tyO2 + dev_looking_forward_constants->C1[6]*tyO4 + dev_looking_forward_constants->C1[7]*tyO6 + dev_looking_forward_constants->C1[8]*txO*tyO2 +dev_looking_forward_constants->C1[9]*txO*tyO4 +dev_looking_forward_constants->C1[10]*txO*tyO6 +dev_looking_forward_constants->C1[11]*txO3*tyO2 + dev_looking_forward_constants->C1[12]*txO3*tyO4 + dev_looking_forward_constants->C1[13]*txO5*tyO2;
-  const float C2 = dev_looking_forward_constants->C2[0]+dev_looking_forward_constants->C2[1]*txO2 + dev_looking_forward_constants->C2[2]*txO4 + dev_looking_forward_constants->C2[3]*tyO2 + dev_looking_forward_constants->C2[4]*tyO4  + dev_looking_forward_constants->C2[5]*txO2*tyO2  + dev_looking_forward_constants->C2[6]*txO6 + dev_looking_forward_constants->C2[7]*tyO5 + dev_looking_forward_constants->C2[8]*txO4*tyO2 + dev_looking_forward_constants->C2[9]*txO2*tyO4;
-  const float C3 = dev_looking_forward_constants->C3[0]+dev_looking_forward_constants->C3[1]*txO + dev_looking_forward_constants->C3[2]*txO3 +dev_looking_forward_constants->C3[3]*txO5 +dev_looking_forward_constants->C3[4]*txO7 + dev_looking_forward_constants->C3[5]*tyO2 + dev_looking_forward_constants->C3[6]*tyO4 + dev_looking_forward_constants->C3[7]*tyO6 + dev_looking_forward_constants->C3[8]*txO*tyO2 +dev_looking_forward_constants->C3[9]*txO*tyO4 +dev_looking_forward_constants->C3[10]*txO*tyO6 +dev_looking_forward_constants->C3[11]*txO3*tyO2 + dev_looking_forward_constants->C3[12]*txO3*tyO4 + dev_looking_forward_constants->C3[13]*txO5*tyO2;
-  const float C4 = dev_looking_forward_constants->C4[0]+dev_looking_forward_constants->C4[1]*txO2 + dev_looking_forward_constants->C4[2]*txO4 + dev_looking_forward_constants->C4[3]*tyO2 + dev_looking_forward_constants->C4[4]*tyO4  + dev_looking_forward_constants->C4[5]*txO2*tyO2  + dev_looking_forward_constants->C4[6]*txO6 + dev_looking_forward_constants->C4[7]*tyO5 + dev_looking_forward_constants->C4[8]*txO4*tyO2 + dev_looking_forward_constants->C4[9]*txO2*tyO4;
+  const auto C0 = dev_looking_forward_constants->C0[0]+dev_looking_forward_constants->C0[1]*txO2 + dev_looking_forward_constants->C0[2]*txO4 + dev_looking_forward_constants->C0[3]*tyO2 + dev_looking_forward_constants->C0[4]*tyO4 + dev_looking_forward_constants->C0[5]*txO2*tyO2 + dev_looking_forward_constants->C0[6]*txO6 + dev_looking_forward_constants->C0[7]*tyO5 + dev_looking_forward_constants->C0[8]*txO4*tyO2 + dev_looking_forward_constants->C0[9]*txO2*tyO4;
+  const auto C1 = dev_looking_forward_constants->C1[0]+dev_looking_forward_constants->C1[1]*txO + dev_looking_forward_constants->C1[2]*txO3 +dev_looking_forward_constants->C1[3]*txO5 +dev_looking_forward_constants->C1[4]*txO7 + dev_looking_forward_constants->C1[5]*tyO2 + dev_looking_forward_constants->C1[6]*tyO4 + dev_looking_forward_constants->C1[7]*tyO6 + dev_looking_forward_constants->C1[8]*txO*tyO2 +dev_looking_forward_constants->C1[9]*txO*tyO4 +dev_looking_forward_constants->C1[10]*txO*tyO6 +dev_looking_forward_constants->C1[11]*txO3*tyO2 + dev_looking_forward_constants->C1[12]*txO3*tyO4 + dev_looking_forward_constants->C1[13]*txO5*tyO2;
+  const auto C2 = dev_looking_forward_constants->C2[0]+dev_looking_forward_constants->C2[1]*txO2 + dev_looking_forward_constants->C2[2]*txO4 + dev_looking_forward_constants->C2[3]*tyO2 + dev_looking_forward_constants->C2[4]*tyO4  + dev_looking_forward_constants->C2[5]*txO2*tyO2  + dev_looking_forward_constants->C2[6]*txO6 + dev_looking_forward_constants->C2[7]*tyO5 + dev_looking_forward_constants->C2[8]*txO4*tyO2 + dev_looking_forward_constants->C2[9]*txO2*tyO4;
+  const auto C3 = dev_looking_forward_constants->C3[0]+dev_looking_forward_constants->C3[1]*txO + dev_looking_forward_constants->C3[2]*txO3 +dev_looking_forward_constants->C3[3]*txO5 +dev_looking_forward_constants->C3[4]*txO7 + dev_looking_forward_constants->C3[5]*tyO2 + dev_looking_forward_constants->C3[6]*tyO4 + dev_looking_forward_constants->C3[7]*tyO6 + dev_looking_forward_constants->C3[8]*txO*tyO2 +dev_looking_forward_constants->C3[9]*txO*tyO4 +dev_looking_forward_constants->C3[10]*txO*tyO6 +dev_looking_forward_constants->C3[11]*txO3*tyO2 + dev_looking_forward_constants->C3[12]*txO3*tyO4 + dev_looking_forward_constants->C3[13]*txO5*tyO2;
+  const auto C4 = dev_looking_forward_constants->C4[0]+dev_looking_forward_constants->C4[1]*txO2 + dev_looking_forward_constants->C4[2]*txO4 + dev_looking_forward_constants->C4[3]*tyO2 + dev_looking_forward_constants->C4[4]*tyO4  + dev_looking_forward_constants->C4[5]*txO2*tyO2  + dev_looking_forward_constants->C4[6]*txO6 + dev_looking_forward_constants->C4[7]*tyO5 + dev_looking_forward_constants->C4[8]*txO4*tyO2 + dev_looking_forward_constants->C4[9]*txO2*tyO4;
 
-  const float MAGFIELD_updated = MAGFIELD * magSign * ( C0 + C1 * DSLOPE + C2 * DSLOPE*DSLOPE + C3 * DSLOPE*DSLOPE*DSLOPE + C4 * DSLOPE*DSLOPE*DSLOPE*DSLOPE);
-  const float qop = DSLOPE / MAGFIELD_updated;
+  const auto MAGFIELD_updated = MAGFIELD * magSign * ( C0 + C1 * DSLOPE + C2 * DSLOPE*DSLOPE + C3 * DSLOPE*DSLOPE*DSLOPE + C4 * DSLOPE*DSLOPE*DSLOPE*DSLOPE);
+  const auto qop = DSLOPE / MAGFIELD_updated;
   return qop;
 }
+
 __global__ void scifi_consolidate_tracks::scifi_consolidate_tracks(
   scifi_consolidate_tracks::Parameters parameters,
   const LookingForward::Constants* dev_looking_forward_constants, 
@@ -182,7 +182,7 @@ __global__ void scifi_consolidate_tracks::scifi_consolidate_tracks(
 
     auto consolidated_hits = scifi_tracks.get_hits(parameters.dev_scifi_track_hits, i);
     const SciFi::TrackHits& track = event_scifi_tracks[i];
-    const float magSign = dev_magnet_polarity[0];
+    const auto magSign = dev_magnet_polarity[0];
     const auto z0 = LookingForward::z_mid_t;
     const auto xVelo = velo_state.x;
     const auto yVelo = velo_state.y;
@@ -193,7 +193,6 @@ __global__ void scifi_consolidate_tracks::scifi_consolidate_tracks(
     // update qop
     scifi_tracks.qop(i) = qop_calculation(
       dev_looking_forward_constants,
-      scifi_tracks,
       magSign,
       z0,
       x0,
