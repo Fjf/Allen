a1ac05b4ee3c25677107af911da45c621b1cda1d
Daniel Campora <dcampora@cern.ch>
2021-01-12 19:53:30 +0100

    Documented event_list_utils functions.

--- configuration/sequences/definitions/event_list_utils.py
+++ configuration/sequences/definitions/event_list_utils.py
@@ -17,11 +17,13 @@ def make_algorithm(alg_type, name, **kwargs):
     """
     Makes an Algorithm with a weight extracted from the benchmark weights.
     
-    In order to determine the weight of Allen algorithms, a profiled benchmark is used for
-    most entries. Each algorithm has an entry in the file `allen_benchmarks.py`, with the
-    exception of:
+    In order to determine the weight of Allen algorithms, a profiled benchmark of `nsys profile`
+    is used for most entries, where the field average time is used.
+    Each algorithm has an entry in the file `allen_benchmarks.py`, with the exception of:
 
-    * Prefix sum algorithms (identified by prefix_sum in its name): Manually 
+    * Prefix sum algorithms (identified by prefix_sum in its name): A weight of 1000.0 is manually set.
+    * Algorithms of category SelectionAlgorithm: A weight of 10.0 is set.
+    * Else: A weight of 100.0 is set. A message is shown identifying the algorithm with no weight.
     """
     if name in benchmark_weights:
         weight = benchmark_weights[name]
@@ -36,6 +38,11 @@ def make_algorithm(alg_type, name, **kwargs):
 
 
 def make_leaf(name, alg, **kwargs):
+    """
+    Makes a leaf identified by its name and algorithm. Also, sets the "efficiency" of the leaf.
+    The efficiency of the leaf indicates how likely is it to accept events. Similarly to
+    benchmark weights, benchmark efficiencies are also set in the file `allen_benchmarks.py`.
+    """
     t = alg.type.getType()
     # TODO efficiency should not be based on the type, but on the configuration (maybe the name)
     if t in benchmark_efficiencies:
@@ -51,6 +58,14 @@ def initialize_event_lists(**kwargs):
 
 
 def add_event_list_combiners(order):
+    """
+    This function accepts an ordered list of algorithms and inserts event list combiners
+    where necessary to fulfill combined masks formed as a combination of other masks.
+
+    Combiners are host algorithms that provide three operations for masks: union, intersection
+    and difference. These three operations would correspond in a single-event scenario to the
+    OR, AND and NOT gate. This equivalence is used to transform NodeLogic into combiners.
+    """
     # gather all combinations that have to be made
     seq = order[0]
     trees = tuple(set([s[1] for s in seq]))
