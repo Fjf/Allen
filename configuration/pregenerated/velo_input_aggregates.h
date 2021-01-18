6fb9b4acc9cffea9f4bae9f8abfbdd5121a9dc7d
lbian <lingzhu.bian@cern.ch>
2021-01-18 16:13:40 +0800

    update pregenerated sequences

--- configuration/pregenerated/velo_input_aggregates.h
+++ configuration/pregenerated/velo_input_aggregates.h
@@ -1,27 +1,9 @@
-/*****************************************************************************\
-* (c) Copyright 2021 CERN for the benefit of the LHCb Collaboration           *
-\*****************************************************************************/
 #pragma once
 
 #include <tuple>
 
-namespace gather_selections {
-  namespace dev_input_selections_t {
-    using tuple_t = std::tuple<>;
-  }
-} // namespace gather_selections
-namespace gather_selections {
-  namespace dev_input_selections_offsets_t {
-    using tuple_t = std::tuple<>;
-  }
-} // namespace gather_selections
-namespace gather_selections {
-  namespace host_input_post_scale_factors_t {
-    using tuple_t = std::tuple<>;
-  }
-} // namespace gather_selections
-namespace gather_selections {
-  namespace host_input_post_scale_hashes_t {
-    using tuple_t = std::tuple<>;
-  }
-} // namespace gather_selections
+
+namespace gather_selections { namespace dev_input_selections_t { using tuple_t = std::tuple<>; }}
+namespace gather_selections { namespace dev_input_selections_offsets_t { using tuple_t = std::tuple<>; }}
+namespace gather_selections { namespace host_input_post_scale_factors_t { using tuple_t = std::tuple<>; }}
+namespace gather_selections { namespace host_input_post_scale_hashes_t { using tuple_t = std::tuple<>; }}
