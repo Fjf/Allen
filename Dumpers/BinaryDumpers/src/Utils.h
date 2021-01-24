9e8951989000dab4f7f74dc096193a3334f6a6a2
Gitlab CI <noreply@cern.ch>
2021-01-24 14:13:00 +0000

    Fixed formatting
    patch generated by https://gitlab.cern.ch/lhcb/Allen/-/jobs/11775423

--- Dumpers/BinaryDumpers/src/Utils.h
+++ Dumpers/BinaryDumpers/src/Utils.h
@@ -21,7 +21,9 @@
 template<class Tuple, class F>
 void for_each(Tuple&& tup, F&& f)
 {
-  std::apply( [f=std::forward<F>(f)](auto&&... args) { (std::invoke(f,std::forward<decltype(args)>(args)),...); }, std::forward<Tuple>(tup) );
+  std::apply(
+    [f = std::forward<F>(f)](auto&&... args) { (std::invoke(f, std::forward<decltype(args)>(args)), ...); },
+    std::forward<Tuple>(tup));
 }
 
 template<class T>
