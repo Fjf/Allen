ca043245f04e7bc629eeed4625d5f5354c7a6929
Gitlab CI <noreply@cern.ch>
2021-02-01 08:01:05 +0000

    Fixed formatting
    patch generated by https://gitlab.cern.ch/lhcb/Allen/-/jobs/11897332

--- test/unit_tests/generic/src/TestPrefixSum.cu
+++ test/unit_tests/generic/src/TestPrefixSum.cu
@@ -16,13 +16,13 @@ TEST_CASE("Test host prefix sum", "[HostPrefixSum]")
   output_v[0] = 0;
   std::partial_sum(v.begin(), v.end() - 1, output_v.begin() + 1);
   const unsigned expected_sum = output_v.back();
-  
+
   // Use host_prefix_sum_impl
   unsigned sum;
   host_prefix_sum::host_prefix_sum_impl(v.data(), v.size() - 1, &sum);
 
   REQUIRE(sum == expected_sum);
-  REQUIRE([&](){
+  REQUIRE([&]() {
     for (size_t i = 0; i < v.size(); ++i) {
       if (v[i] != output_v[i]) {
         return false;
