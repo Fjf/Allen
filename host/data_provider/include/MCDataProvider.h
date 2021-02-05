493c4b9fc7c69c3b6fbffccaf5646fcdb8cb4f08
Daniel Campora <dcampora@cern.ch>
2021-02-05 10:58:36 +0100

    Added MCDataProvider algorithm.

---
+++ host/data_provider/include/MCDataProvider.h
@@ -0,0 +1,30 @@
+/*****************************************************************************\
+* (c) Copyright 2018-2020 CERN for the benefit of the LHCb Collaboration      *
+\*****************************************************************************/
+#pragma once
+
+#include "Common.h"
+#include "HostAlgorithm.cuh"
+#include "InputProvider.h"
+#include "MCEvent.h"
+
+namespace mc_data_provider {
+  struct Parameters {
+    HOST_OUTPUT(host_mc_events_t, MCEvents) host_mc_events;
+  };
+
+  struct mc_data_provider_t : public HostAlgorithm, Parameters {
+    void set_arguments_size(
+      ArgumentReferences<Parameters>,
+      const RuntimeOptions&,
+      const Constants&,
+      const HostBuffers&) const;
+
+    void operator()(
+      const ArgumentReferences<Parameters>&,
+      const RuntimeOptions&,
+      const Constants&,
+      HostBuffers&,
+      const Allen::Context&) const;
+  };
+} // namespace mc_data_provider
