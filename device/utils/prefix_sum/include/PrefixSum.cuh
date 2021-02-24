617a92a54abbc7b87f76ff8b2ff9bb99dc7e3a8d
Gitlab CI <noreply@cern.ch>
2021-02-24 15:17:29 +0000

    Fixed formatting

--- device/utils/prefix_sum/include/PrefixSum.cuh
+++ device/utils/prefix_sum/include/PrefixSum.cuh
@@ -8,11 +8,8 @@
 #include "SciFiEventModel.cuh"
 #include "UTEventModel.cuh"
 
-__global__ void
-  prefix_sum_reduce(unsigned* dev_main_array, unsigned* dev_auxiliary_array, const unsigned array_size);
+__global__ void prefix_sum_reduce(unsigned* dev_main_array, unsigned* dev_auxiliary_array, const unsigned array_size);
 
-__global__ void
-  prefix_sum_single_block(unsigned* dev_total_sum, unsigned* dev_array, const unsigned array_size);
+__global__ void prefix_sum_single_block(unsigned* dev_total_sum, unsigned* dev_array, const unsigned array_size);
 
-__global__ void
-  prefix_sum_scan(unsigned* dev_main_array, unsigned* dev_auxiliary_array, const unsigned array_size);
+__global__ void prefix_sum_scan(unsigned* dev_main_array, unsigned* dev_auxiliary_array, const unsigned array_size);
