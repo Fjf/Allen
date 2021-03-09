cb4c2d43044a9c183a095fe313a141fd2951a181
Daniel Campora <dcampora@cern.ch>
2021-03-09 19:39:40 +0100

    Added copyright.

--- host/combiners/src/EventListInversion.cpp
+++ host/combiners/src/EventListInversion.cpp
@@ -1,3 +1,13 @@
+/*****************************************************************************\
+* (c) Copyright 2021 CERN for the benefit of the LHCb Collaboration           *
+*                                                                             *
+* This software is distributed under the terms of the Apache License          *
+* version 2 (Apache-2.0), copied verbatim in the file "COPYING".              *
+*                                                                             *
+* In applying this licence, CERN does not waive the privileges and immunities *
+* granted to it by virtue of its status as an Intergovernmental Organization  *
+* or submit itself to any jurisdiction.                                       *
+\*****************************************************************************/
 #include "EventListInversion.cuh"
 
 void event_list_inversion::event_list_inversion_t::set_arguments_size(
--- host/init_event_list/src/HostInitNumberOfEvents.cpp
+++ host/init_event_list/src/HostInitNumberOfEvents.cpp
@@ -1,3 +1,13 @@
+/*****************************************************************************\
+* (c) Copyright 2021 CERN for the benefit of the LHCb Collaboration           *
+*                                                                             *
+* This software is distributed under the terms of the Apache License          *
+* version 2 (Apache-2.0), copied verbatim in the file "COPYING".              *
+*                                                                             *
+* In applying this licence, CERN does not waive the privileges and immunities *
+* granted to it by virtue of its status as an Intergovernmental Organization  *
+* or submit itself to any jurisdiction.                                       *
+\*****************************************************************************/
 #include "HostInitNumberOfEvents.h"
 
 void host_init_number_of_events::host_init_number_of_events_t::set_arguments_size(
