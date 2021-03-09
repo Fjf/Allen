f6460e0fae1db0e56be6acedb185feb023e442c1
Daniel Campora <dcampora@cern.ch>
2021-03-09 17:23:32 +0100

    Use GIT_EXECUTABLE in GenerateConfiguration.

--- cmake/GenerateConfiguration.cmake
+++ cmake/GenerateConfiguration.cmake
@@ -56,8 +56,8 @@ if (Python3_FOUND AND (LIBCLANG_FOUND OR LIBCLANG_ALTERNATIVE_FOUND) AND SEQUENC
     add_custom_command(
       OUTPUT "${PROJECT_SEQUENCE_DIR}/PyConf"
       COMMAND
-        ${CMAKE_COMMAND} -E env git clone https://gitlab.cern.ch/lhcb/LHCb --no-checkout &&
-        ${CMAKE_COMMAND} -E env git -C LHCb/ checkout origin/dcampora_nnolte_mes_pyconf -- PyConf &&
+        ${CMAKE_COMMAND} -E env ${GIT_EXECUTABLE} clone https://gitlab.cern.ch/lhcb/LHCb --no-checkout &&
+        ${CMAKE_COMMAND} -E env ${GIT_EXECUTABLE} -C LHCb/ checkout origin/dcampora_nnolte_mes_pyconf -- PyConf &&
         ${CMAKE_COMMAND} -E create_symlink LHCb/PyConf/python/PyConf PyConf
       WORKING_DIRECTORY ${PROJECT_SEQUENCE_DIR})
     add_custom_command(
