c100cf66bdb3c2ddae871978c95ca2c317e6a963
Daniel Campora <dcampora@cern.ch>
2021-03-15 16:50:47 +0100

    Add copyright.

--- configuration/tests/test_event_list_utils.py
+++ configuration/tests/test_event_list_utils.py
@@ -1,3 +1,13 @@
+###############################################################################
+# (c) Copyright 2021 CERN for the benefit of the LHCb Collaboration           #
+#                                                                             #
+# This software is distributed under the terms of the Apache License          #
+# version 2 (Apache-2.0), copied verbatim in the file "COPYING".              #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
 from PyConf.components import Algorithm
 from AllenConf.event_list_utils import add_event_list_combiners, initialize_event_lists
 from AllenConf.cftree_ops import get_execution_list_for, parse_boolean
