643ca314c309f27d6d60cd8a60c7fbcea676d5fa
Niklas Nolte <niklas.nolte@cern.ch>
2021-03-18 10:50:38 +0100

    move test and remove it from gitlabci

--- configuration/tests/test_configuration.sh
+++ configuration/tests/test_configuration.sh
@@ -10,14 +10,19 @@
 # or submit itself to any jurisdiction.                                       #
 ###############################################################################
 
+# Prepare tests in a separate dir
 TEST_DIR=$(mktemp -d)
 cp -r ${1}/sequences ${TEST_DIR}
 cp ${1}/tests/test_algorithms.py ${TEST_DIR}/sequences/definitions/algorithms.py
 cp -r ${1}/AllenConf ${TEST_DIR}/sequences
 cp -r ${1}/tests ${TEST_DIR}/sequences
 cd ${TEST_DIR}/sequences
-rm ${TEST_DIR}/sequences/tests/test_algorithms.py
+rm ${TEST_DIR}/sequences/tests/test_algorithms.py ${TEST_DIR}/sequences/tests/test_configuration.sh
+
+# Run tests
 pytest -s tests
 
-echo ${TEST_DIR}
-# rm -rf ${TEST_DIR}
+# Cleanup and exit
+exit_code=$?
+rm -rf ${TEST_DIR}
+exit $exit_code
