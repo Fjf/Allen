7ddf7d7a71f9130957b6bccc9ae9c8312d4bc43c
Daniel Campora <dcampora@cern.ch>
2021-03-25 09:41:46 +0100

    Use HIP_DYNAMIC_SHARED to configure extern shared memory in HIP.

--- backend/include/HIPBackend.h
+++ backend/include/HIPBackend.h
@@ -45,7 +45,7 @@
 #define half_t half
 
 // Support for dynamic shared memory buffers
-#define DYNAMIC_SHARED_MEMORY_BUFFER(__type, __instance, __config) extern __shared__ __type __instance[];
+#define DYNAMIC_SHARED_MEMORY_BUFFER(__type, __instance, __config) HIP_DYNAMIC_SHARED(__type, __instance)
 
 // syncwarp is not supported in HIP. Use syncthreads instead
 #define __syncwarp __syncthreads
