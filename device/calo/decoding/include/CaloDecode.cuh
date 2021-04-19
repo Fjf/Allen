dfaf947ca4ea6ec9fd534bcbbfd6bd3ea0338ce8
Roel Aaij <roel.aaij@cern.ch>
2021-04-19 21:48:08 +0200

    Use template to reduce number of functions

--- device/calo/decoding/include/CaloDecode.cuh
+++ device/calo/decoding/include/CaloDecode.cuh
@@ -10,6 +10,72 @@
 #include "CaloDigit.cuh"
 #include "DeviceAlgorithm.cuh"
 
+namespace {
+  template<typename RawEvent>
+  __device__ void decode(
+    const char* event_data,
+    const uint32_t* event_offsets,
+    unsigned const event_number,
+    CaloDigit* digits,
+    unsigned const number_of_digits,
+    CaloGeometry const& geometry)
+  {
+    auto raw_event = RawEvent {event_data, event_offsets};
+    for (unsigned bank_number = threadIdx.x; bank_number < raw_event.number_of_raw_banks; bank_number += blockDim.x) {
+      auto raw_bank = raw_event.bank(event_number, bank_number);
+      while (raw_bank.data < raw_bank.end) {
+        uint32_t word = *raw_bank.data;
+        uint16_t trig_size = word & 0x7F;
+        uint16_t code = (word >> 14) & 0x1FF;
+
+        // Skip header and trigger words
+        raw_bank.data += 1 + (trig_size + 3) / 4;
+
+        // pattern bits
+        unsigned int pattern = *raw_bank.data;
+        // Loop over all cards in this front-env sub-bank.
+        uint32_t last_data = *(raw_bank.data + 1);
+        raw_bank.data += 2;
+
+        int16_t offset = 0;
+
+        for (unsigned int bit_num = 0; 32 > bit_num; ++bit_num) {
+          if (31 < offset) {
+            offset -= 32;
+            last_data = *raw_bank.data;
+            raw_bank.data += 1;
+          }
+          int adc;
+          if (0 == (pattern & (1 << bit_num))) { //.. short coding
+            adc = ((last_data >> offset) & 0xF) - 8;
+            offset += 4;
+          }
+          else {
+            adc = ((last_data >> offset) & 0xFFF);
+            if (24 == offset) adc &= 0xFF;
+            if (28 == offset) adc &= 0xF; //== clean-up extra bits
+            offset += 12;
+            if (32 < offset) { //.. get the extra bits on next word
+              last_data = *raw_bank.data;
+              raw_bank.data += 1;
+              offset -= 32;
+              int temp = (last_data << (12 - offset)) & 0xFFF;
+              adc += temp;
+            }
+            adc -= 256;
+          }
+
+          uint16_t index = geometry.channels[(code - geometry.code_offset) * geometry.card_channels + bit_num];
+          // Ignore cells with invalid indices; these include LED diodes.
+          if (index < number_of_digits) {
+            digits[index].adc = adc;
+          }
+        }
+      }
+    }
+  }
+}
+
 namespace calo_decode {
   struct Parameters {
     HOST_INPUT(host_number_of_events_t, unsigned) host_number_of_events;
@@ -35,9 +101,39 @@ namespace calo_decode {
       const Allen::Context&) const;
   };
 
-  // Global function
-  __global__ void calo_decode(Parameters parameters, const char* dev_ecal_geometry, const char* dev_hcal_geometry);
-  __global__ void calo_decode_mep(Parameters parameters, const char* dev_ecal_geometry, const char* dev_hcal_geometry);
+  // Decode dispatch
+  template<bool MEP>
+  __global__ void calo_decode(
+    calo_decode::Parameters parameters,
+    const char* raw_ecal_geometry,
+    const char* raw_hcal_geometry)
+  {
+    using RawEvent = std::conditional_t<MEP, CaloMepEvent, CaloRawEvent>;
+
+    unsigned const event_number = parameters.dev_event_list[blockIdx.x];
+
+    // ECal
+    auto ecal_geometry = CaloGeometry(raw_ecal_geometry);
+    auto const ecal_digits_offset = parameters.dev_ecal_digits_offsets[event_number];
+    decode<RawEvent>(
+      parameters.dev_ecal_raw_input,
+      parameters.dev_ecal_raw_input_offsets,
+      event_number,
+      &parameters.dev_ecal_digits[ecal_digits_offset],
+      parameters.dev_ecal_digits_offsets[event_number + 1] - ecal_digits_offset,
+      ecal_geometry);
+
+    // HCal
+    auto hcal_geometry = CaloGeometry(raw_hcal_geometry);
+    auto const hcal_digits_offset = parameters.dev_hcal_digits_offsets[event_number];
+    decode<RawEvent>(
+      parameters.dev_hcal_raw_input,
+      parameters.dev_hcal_raw_input_offsets,
+      event_number,
+      &parameters.dev_hcal_digits[hcal_digits_offset],
+      parameters.dev_hcal_digits_offsets[event_number + 1] - hcal_digits_offset,
+      hcal_geometry);
+  }
 
   // Algorithm
   struct calo_decode_t : public DeviceAlgorithm, Parameters {
