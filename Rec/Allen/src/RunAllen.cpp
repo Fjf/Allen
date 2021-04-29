9bd55139ff47104baa20ed445863d0e44e85aea6
Thomas Boettcher <boettcts@ucmail.uc.edu>
2021-04-29 20:55:01 +0000

    Add minimal two-track catboost line

--- Rec/Allen/src/RunAllen.cpp
+++ Rec/Allen/src/RunAllen.cpp
@@ -77,6 +77,17 @@ StatusCode RunAllen::initialize()
     muon_catboost_model_reader->split_border(),
     muon_catboost_model_reader->split_feature());
 
+  std::unique_ptr<CatboostModelReader> two_track_catboost_model_reader =
+    std::make_unique<CatboostModelReader>(geometry_path + "/two_track_catboost_model_small.json");
+  m_constants.initialize_two_track_catboost_model_constants(
+    two_track_catboost_model_reader->n_trees(),
+    two_track_catboost_model_reader->tree_depths(),
+    two_track_catboost_model_reader->tree_offsets(),
+    two_track_catboost_model_reader->leaf_values(),
+    two_track_catboost_model_reader->leaf_offsets(),
+    two_track_catboost_model_reader->split_border(),
+    two_track_catboost_model_reader->split_feature());
+
   // Allen Consumers
   register_consumers(updater, m_constants);
 
