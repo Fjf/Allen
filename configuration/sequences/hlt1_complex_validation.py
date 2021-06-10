2b31897b04a4726d69ebcc27bbb2c6daa72415a1
Dorothea vom Bruch <dorothea.vom.bruch@cern.ch>
2021-06-10 22:14:54 +0200

    update complex sequence configuration to revisited line_maker scheme

--- configuration/sequences/hlt1_complex_validation.py
+++ configuration/sequences/hlt1_complex_validation.py
@@ -3,10 +3,10 @@
 ###############################################################################
 from AllenConf.utils import gec
 from AllenConf.ut_reconstruction import make_ut_tracks
-from AllenConf.persistency import make_gather_selections, make_dec_reporter
+from AllenConf.persistency import make_gather_selections, make_global_decision
 from AllenConf.hlt1_reconstruction import hlt1_reconstruction
 from AllenConf.hlt1_inclusive_hadron_lines import make_track_mva_line, make_two_track_mva_line
-from AllenConf.HLT1 import make_line_composite_node_with_gec
+from AllenConf.HLT1 import line_maker
 from AllenConf.validators import (
     velo_validation, veloUT_validation, forward_validation, muon_validation,
     pv_validation, rate_validation, kalman_validation)
@@ -20,59 +20,53 @@ with make_ut_tracks.bind(restricted=False):
 
 restricted_hlt1_reconstruction = hlt1_reconstruction()
 
-# Line AllenConf
-line_algorithms = {
-    "Hlt1TrackMVA_Restricted":
-    make_track_mva_line(
-        restricted_hlt1_reconstruction["forward_tracks"],
-        restricted_hlt1_reconstruction["secondary_vertices"],
-        name="Hlt1TrackMVA_Restricted"),
-    "Hlt1TwoTrackMVA_Restricted":
-    make_two_track_mva_line(
-        restricted_hlt1_reconstruction["forward_tracks"],
-        restricted_hlt1_reconstruction["secondary_vertices"],
-        name="Hlt1TwoTrackMVA_Restricted"),
-    "Hlt1TrackMVA_Non_Restricted":
-    make_track_mva_line(
-        non_restricted_hlt1_reconstruction["forward_tracks"],
-        non_restricted_hlt1_reconstruction["secondary_vertices"],
-        name="Hlt1TrackMVA_Non_Restricted"),
-    "Hlt1TwoTrackMVA_Non_Restricted":
-    make_two_track_mva_line(
-        non_restricted_hlt1_reconstruction["forward_tracks"],
-        non_restricted_hlt1_reconstruction["secondary_vertices"],
-        name="Hlt1TwoTrackMVA_Non_Restricted"),
-}
+lines = []
+lines.append(
+    line_maker(
+        "Hlt1TrackMVA_Restricted",
+        make_track_mva_line(restricted_hlt1_reconstruction["forward_tracks"],
+                            restricted_hlt1_reconstruction["kalman_velo_only"],
+                            name="Hlt1TrackMVA_Restricted"),
+        enableGEC=True))  
+lines.append(
+    line_maker(
+        "Hlt1TwoTrackMVA_Restricted",
+        make_two_track_mva_line(restricted_hlt1_reconstruction["forward_tracks"],
+                                restricted_hlt1_reconstruction["secondary_vertices"],
+                                name="Hlt1TwoTrackMVA_Restricted"),
+        enableGEC=True))
+lines.append(
+    line_maker(
+        "Hlt1TrackMVA_Non_Restricted",
+        make_track_mva_line(
+            non_restricted_hlt1_reconstruction["forward_tracks"],
+            non_restricted_hlt1_reconstruction["secondary_vertices"],
+            name="Hlt1TrackMVA_Non_Restricted"), 
+        enableGEC=True))
+lines.append(
+    line_maker(
+        "Hlt1TwoTrackMVA_Non_Restricted",
+        make_two_track_mva_line(
+            non_restricted_hlt1_reconstruction["forward_tracks"],
+            non_restricted_hlt1_reconstruction["secondary_vertices"],
+            name="Hlt1TwoTrackMVA_Non_Restricted"), 
+        enableGEC=True))
+        
+# list of line algorithms, required for the gather selection and DecReport algorithms
+line_algorithms = [tup[0] for tup in lines]
 
-track_mva_line_restricted = make_line_composite_node_with_gec(
-    "Hlt1TrackMVA_Restricted", line_algorithms["Hlt1TrackMVA_Restricted"])
-two_track_mva_line_restricted = make_line_composite_node_with_gec(
-    "Hlt1TwoTrackMVA_Restricted",
-    line_algorithms["Hlt1TwoTrackMVA_Restricted"])
-track_mva_line_non_restricted = make_line_composite_node_with_gec(
-    "Hlt1TrackMVA_Non_Restricted",
-    line_algorithms["Hlt1TrackMVA_Non_Restricted"])
-two_track_mva_line_non_restricted = make_line_composite_node_with_gec(
-    "Hlt1TwoTrackMVA_Non_Restricted",
-    line_algorithms["Hlt1TwoTrackMVA_Non_Restricted"])
+# lost of line nodes, required to set up the CompositeNode
+line_nodes = [tup[1] for tup in lines] 
 
 lines_leaf = CompositeNode(
-    "AllLines", [
-        track_mva_line_restricted, two_track_mva_line_restricted,
-        track_mva_line_non_restricted, two_track_mva_line_non_restricted
-    ],
-    NodeLogic.NONLAZY_OR,
-    force_order=False)
+    "AllLines", line_nodes, NodeLogic.NONLAZY_OR, force_order=False)
 
-hlt1_node = CompositeNode(
-    "Allen", [
-        lines_leaf,
-        make_dec_reporter(lines=line_algorithms.values()),
-    ],
+hlt1_leaf = CompositeNode(
+    "Allen", [lines_leaf, make_global_decision(lines=line_algorithms)],
     NodeLogic.NONLAZY_AND,
     force_order=True)
 
-validators_node = CompositeNode(
+validators_leaf = CompositeNode(
     "Validators", [
         velo_validation(restricted_hlt1_reconstruction["velo_tracks"]),
         veloUT_validation(
@@ -95,7 +89,7 @@ validators_node = CompositeNode(
             name="non-restricted_muon_validation"),
         pv_validation(restricted_hlt1_reconstruction["pvs"]),
         rate_validation(
-            make_gather_selections(lines=line_algorithms.values())),
+            make_gather_selections(line_algorithms)),
         kalman_validation(
             restricted_hlt1_reconstruction["kalman_velo_only"],
             name="restricted_kalman_validation")
@@ -104,7 +98,7 @@ validators_node = CompositeNode(
     force_order=False)
 
 node = CompositeNode(
-    "AllenWithValidators", [hlt1_node, validators_node],
+    "AllenWithValidators", [hlt1_leaf, validators_leaf],
     NodeLogic.NONLAZY_AND,
     force_order=False)
 
