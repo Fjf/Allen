c47acd4a959615105506224c66bbe25ce091ed9a
Gitlab CI <noreply@cern.ch>
2021-06-10 20:16:39 +0000

    Fixed formatting
    patch generated by https://gitlab.cern.ch/lhcb/Allen/-/jobs/14434501

--- configuration/sequences/hlt1_complex_validation.py
+++ configuration/sequences/hlt1_complex_validation.py
@@ -24,16 +24,18 @@ lines = []
 lines.append(
     line_maker(
         "Hlt1TrackMVA_Restricted",
-        make_track_mva_line(restricted_hlt1_reconstruction["forward_tracks"],
-                            restricted_hlt1_reconstruction["kalman_velo_only"],
-                            name="Hlt1TrackMVA_Restricted"),
-        enableGEC=True))  
+        make_track_mva_line(
+            restricted_hlt1_reconstruction["forward_tracks"],
+            restricted_hlt1_reconstruction["kalman_velo_only"],
+            name="Hlt1TrackMVA_Restricted"),
+        enableGEC=True))
 lines.append(
     line_maker(
         "Hlt1TwoTrackMVA_Restricted",
-        make_two_track_mva_line(restricted_hlt1_reconstruction["forward_tracks"],
-                                restricted_hlt1_reconstruction["secondary_vertices"],
-                                name="Hlt1TwoTrackMVA_Restricted"),
+        make_two_track_mva_line(
+            restricted_hlt1_reconstruction["forward_tracks"],
+            restricted_hlt1_reconstruction["secondary_vertices"],
+            name="Hlt1TwoTrackMVA_Restricted"),
         enableGEC=True))
 lines.append(
     line_maker(
@@ -41,7 +43,7 @@ lines.append(
         make_track_mva_line(
             non_restricted_hlt1_reconstruction["forward_tracks"],
             non_restricted_hlt1_reconstruction["secondary_vertices"],
-            name="Hlt1TrackMVA_Non_Restricted"), 
+            name="Hlt1TrackMVA_Non_Restricted"),
         enableGEC=True))
 lines.append(
     line_maker(
@@ -49,20 +51,21 @@ lines.append(
         make_two_track_mva_line(
             non_restricted_hlt1_reconstruction["forward_tracks"],
             non_restricted_hlt1_reconstruction["secondary_vertices"],
-            name="Hlt1TwoTrackMVA_Non_Restricted"), 
+            name="Hlt1TwoTrackMVA_Non_Restricted"),
         enableGEC=True))
-        
+
 # list of line algorithms, required for the gather selection and DecReport algorithms
 line_algorithms = [tup[0] for tup in lines]
 
 # lost of line nodes, required to set up the CompositeNode
-line_nodes = [tup[1] for tup in lines] 
+line_nodes = [tup[1] for tup in lines]
 
 lines_leaf = CompositeNode(
     "AllLines", line_nodes, NodeLogic.NONLAZY_OR, force_order=False)
 
 hlt1_leaf = CompositeNode(
-    "Allen", [lines_leaf, make_global_decision(lines=line_algorithms)],
+    "Allen",
+    [lines_leaf, make_global_decision(lines=line_algorithms)],
     NodeLogic.NONLAZY_AND,
     force_order=True)
 
@@ -88,8 +91,7 @@ validators_leaf = CompositeNode(
             non_restricted_hlt1_reconstruction["muonID"],
             name="non-restricted_muon_validation"),
         pv_validation(restricted_hlt1_reconstruction["pvs"]),
-        rate_validation(
-            make_gather_selections(line_algorithms)),
+        rate_validation(make_gather_selections(line_algorithms)),
         kalman_validation(
             restricted_hlt1_reconstruction["kalman_velo_only"],
             name="restricted_kalman_validation")
