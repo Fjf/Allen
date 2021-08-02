18ab97d243424021300bb1c9f4797a129e2c10f2
Gitlab CI <noreply@cern.ch>
2021-08-02 09:45:01 +0000

    Fixed formatting
    patch generated by https://gitlab.cern.ch/lhcb/Allen/-/jobs/15507744

--- device/velo/search_by_triplet/src/SearchByTriplet.cu
+++ device/velo/search_by_triplet/src/SearchByTriplet.cu
@@ -281,12 +281,11 @@ __device__ std::tuple<int16_t, int16_t> velo_search_by_triplet::find_forward_can
   const auto y_prediction = h0.y + predy;
   const auto track_extrapolation_phi = hit_phi_16(x_prediction, y_prediction);
 
-  return {
-    binary_search_leftmost(
-      hit_phis + module_pair.hit_start,
-      module_pair.hit_num,
-      static_cast<int16_t>(track_extrapolation_phi - phi_tolerance)),
-    track_extrapolation_phi};
+  return {binary_search_leftmost(
+            hit_phis + module_pair.hit_start,
+            module_pair.hit_num,
+            static_cast<int16_t>(track_extrapolation_phi - phi_tolerance)),
+          track_extrapolation_phi};
 }
 
 /**
