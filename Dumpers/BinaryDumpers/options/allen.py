330fa886405f062415182fbfe411a2364c9aac85
Roel Aaij <raaij@nikhef.nl>
2021-08-15 21:18:46 +0200

    Allow using MEPProvider for allen.py

--- Dumpers/BinaryDumpers/options/allen.py
+++ Dumpers/BinaryDumpers/options/allen.py
@@ -3,10 +3,12 @@
 # (c) Copyright 2018-2021 CERN for the benefit of the LHCb Collaboration      #
 ###############################################################################
 import os
-from functools import partial
+from GaudiPython.Bindings import AppMgr, gbl
 from Configurables import LHCbApp, CondDB, ApplicationMgr
 from Allen.config import setup_allen_non_event_data_service
-from GaudiPython.Bindings import AppMgr, gbl
+from threading import Thread
+from time import sleep
+import ctypes
 import argparse
 
 # Load Allen entry point and helpers
@@ -16,7 +18,7 @@ interpreter = gbl.gInterpreter
 
 # FIXME: Once the headers are installed properly, this should not be
 # necessary anymore
-allen_dir = os.environ['ALLEN_PROJECT_ROOT']
+allen_dir = os.environ["ALLEN_PROJECT_ROOT"]
 interpreter.Declare("#include <Dumpers/IUpdater.h>")
 interpreter.Declare("#include <Allen/Allen.h>")
 interpreter.Declare("#include <Allen/Provider.h>")
@@ -40,15 +42,14 @@ parser.add_argument(
     "--params",
     dest="param_folder",
     default=os.path.join(allen_dir, "input", "parameters"))
-parser.add_argument("-n", dest="n_events", default="0")
-parser.add_argument("-t", dest="threads", default="1")
-parser.add_argument("-r", dest="repetitions", default="1")
-parser.add_argument("-m", dest="reserve", default="1024")
-parser.add_argument("-v", dest="verbosity", default="3")
-parser.add_argument("-p", dest="print_memory", default="0")
-parser.add_argument("-i", dest="import_fwd", default="")
+parser.add_argument("-n", dest="n_events", default=0)
+parser.add_argument("-t", dest="threads", default=1)
+parser.add_argument("-r", dest="repetitions", default=1)
+parser.add_argument("-m", dest="reserve", default=1024)
+parser.add_argument("-v", dest="verbosity", default=3)
+parser.add_argument("-p", dest="print_memory", default=0)
 parser.add_argument("--sequence", dest="sequence", default=sequence_default)
-parser.add_argument("-s", dest="slices", default="2")
+parser.add_argument("-s", dest="slices", default=2)
 parser.add_argument("-b", "--bank-types", dest="bank_types",
                     default="VP,FTCluster,UT,Muon,ODIN")
 parser.add_argument(
@@ -56,30 +57,67 @@ parser.add_argument(
     dest="mdf",
     default=os.path.join(allen_dir, "input", "minbias", "mdf",
                          "upgrade_mc_minbias_scifi_v5.mdf"))
-parser.add_argument("--cpu-offload", dest="cpu_offload", default="1")
 parser.add_argument(
-    "--output-file", dest="output_file", default=None)
+    "--reuse-meps",
+    action="store_true",
+    dest="reuse_meps",
+    default=False,
+    help="Fill all MEP buffers once and then reuse them",
+)
 parser.add_argument(
-    "--monitoring-save-period", dest="mon_save_period", default=0)
+    "--cpu-offload",
+    dest="cpu_offload",
+    default=1,
+    help="Offload some operations to the CPU",
+)
 parser.add_argument(
-    "--monitoring-filename",
-    dest="mon_filename",
-    default="monitoringHists.root", help="Histogram filename")
+    "--profile",
+    dest="profile",
+    type=str,
+    default="",
+    choices=["", "CUDA"],
+    help="Add profiler start and stop calls",
+)
+parser.add_argument("--output-file", dest="output_file", default=None)
+parser.add_argument("--monitoring-save-period", dest="mon_save_period", default=0)
 parser.add_argument(
-    "--disable-run-changes", dest="disable_run_changes", default=1)
+    "--monitoring-filename", dest="mon_filename", default="", help="Histogram filename"
+)
 parser.add_argument(
-    "--events-per-slice", dest="events_per_slice", default=1000,
-    help="number of events per batch submitted to the GPU")
+    "--enable-run-changes",
+    dest="enable_run_changes",
+    action="store_true",
+    default=False,
+)
+parser.add_argument(
+    "--events-per-slice",
+    dest="events_per_slice",
+    default=1000,
+    help="number of events per batch submitted to the GPU",
+)
 parser.add_argument("--device", dest="device", default=0, help="Device index")
+parser.add_argument(
+    "--runtime",
+    dest="runtime",
+    type=int,
+    default=300,
+    help="How long to run when reusing MEPs",
+)
 
 args = parser.parse_args()
+
+runtime_lib = None
+if args.profile == "CUDA":
+    runtime_lib = ctypes.CDLL("libcudart.so")
+
 
 app = LHCbApp(
     DataType="Upgrade",
     EvtMax=1000,
     Simulation=True,
     DDDBtag="dddb-20210218",
-    CondDBtag="sim-20201218-vc-md100")
+    CondDBtag="sim-20201218-vc-md100",
+)
 
 # Upgrade DBs
 CondDB().Upgrade = True
@@ -88,29 +126,34 @@ setup_allen_non_event_data_service()
 
 # Some extra stuff for timing table
 ApplicationMgr().EvtSel = "NONE"
-ApplicationMgr().ExtSvc += ['ToolSvc', 'AuditorSvc', 'ZeroMQSvc']
+ApplicationMgr().ExtSvc += ["ToolSvc", "AuditorSvc", "ZeroMQSvc"]
 
 if args.mep is not None:
-    ApplicationMgr().ExtSvc += ['MEPProvider']
+    ApplicationMgr().ExtSvc += ["MEPProvider"]
     from Configurables import MEPProvider
 
     mep_provider = MEPProvider()
     mep_provider.NSlices = args.slices
-    mep_provider.EventsPerSlice = 1000
-    mep_provider.OutputLevel = 2
+    mep_provider.EventsPerSlice = args.events_per_slice
+    mep_provider.OutputLevel = 3
     # Number of MEP buffers and number of transpose/offset threads
-    mep_provider.BufferConfig = (4, 3)
+    mep_provider.BufferConfig = (10, 8)
     mep_provider.TransposeMEPs = False
     mep_provider.SplitByRun = False
     mep_provider.Source = "Files"
     mep_dir = args.mep
-    mep_provider.Connections = sorted([os.path.join(mep_dir, mep_file)
-                                       for mep_file in os.listdir(mep_dir)
-                                       if mep_file.endswith('.mep')])
+    mep_provider.Connections = sorted(
+        [
+            os.path.join(mep_dir, mep_file)
+            for mep_file in os.listdir(mep_dir)
+            if mep_file.endswith(".mep")
+        ]
+    )
     mep_provider.LoopOnMEPs = False
-    mep_provider.Preload = True
-    mep_provider.EvtMax = 50000
-    mep_provider.BufferNUMA = [0, 0, 1, 1]
+    mep_provider.Preload = args.reuse_meps
+    mep_provider.BufferNUMA = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+    # mep_provider.BankTypes = ['VP', 'FTCluster', 'Muon', 'ODIN', 'UT']
+    mep_provider.EvtMax = -1 if args.n_events == -1 else args.n_events
 
 # Start Gaudi and get the AllenUpdater service
 gaudi = AppMgr()
@@ -122,17 +165,17 @@ updater = cast_service(gbl.Allen.NonEventData.IUpdater, svc)
 
 # options map
 options = gbl.std.map("std::string", "std::string")()
-for flag, value in (("g", args.det_folder), ("params", args.param_folder),
+for flag, value in [("g", args.det_folder), ("params", args.param_folder),
                     ("n", args.n_events),
                     ("t", args.threads), ("r", args.repetitions),
                     ("output-file", args.output_file),
                     ("m", args.reserve), ("v", args.verbosity),
-                    ("p", args.print_memory), ("i", args.import_fwd),
+                    ("p", args.print_memory),
                     ("sequence", args.sequence),
                     ("s", args.slices), ("mdf", args.mdf),
                     ("b", args.bank_types),
                     ("cpu-offload", args.cpu_offload),
-                    ("disable-run-changes", args.disable_run_changes),
+                    ("disable-run-changes", int(not args.enable_run_changes)),
                     ("monitoring-save-period", args.mon_save_period),
                     ("monitoring-filename", args.mon_filename),
                     ("events-per-slice", args.events_per_slice),
@@ -153,4 +196,25 @@ output_handler = gbl.Allen.output_handler(provider, zmqSvc, options)
 gaudi.start()
 
 # run Allen
+gbl.allen.__release_gil__ = 1
+
+
+def sleep_fun():
+    print("sleeping")
+    sleep(args.runtime)
+    gaudi.stop()
+
+
+if args.reuse_meps:
+    sleep_thread = Thread(target=sleep_fun)
+    sleep_thread.start()
+
+
+if args.profile == "CUDA":
+    runtime_lib.cudaProfilerStart()
 gbl.allen(options, updater, provider, output_handler, zmqSvc, con.c_str())
+if args.profile == "CUDA":
+    runtime_lib.cudaProfilerStop()
+
+if args.reuse_meps:
+    sleep_thread.join()
