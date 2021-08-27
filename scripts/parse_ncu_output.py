a0f3777acf25c2665cc6732213f8982717090893
Daniel Campora <dcampora@cern.ch>
2021-08-27 16:38:44 +0200

    Profile using ncu instead of nsys. Use custmo metric. Keep artifacts of profile.

---
+++ scripts/parse_ncu_output.py
@@ -0,0 +1,45 @@
+import csv
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Parses a csv file from a ncu report and generates a csv with a custom metric.'
+)
+
+parser.add_argument(
+    'input_filename',
+    nargs='?',
+    type=str,
+    default="allen_report.csv",
+    help='input filename')
+parser.add_argument(
+    'output_filename',
+    nargs='?',
+    type=str,
+    default="allen_report_custom_metric.csv",
+    help='output filename')
+args = parser.parse_args()
+
+kernel_dict = {}
+with open(args.input_filename) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if row["Kernel Name"] not in kernel_dict:
+            kernel_dict[row["Kernel Name"]] = {}
+        kernel_dict[row["Kernel Name"]][row["Metric Name"]] = row
+
+algorithm_list = []
+for k in kernel_dict.keys():
+    def get_metric(metric, name="Metric Value", cast_lambda=lambda x : float(x.replace(",",""))):
+        return cast_lambda(kernel_dict[k][metric][name])
+
+    custom_metric_1 = get_metric("SM Active Cycles") * \
+        get_metric("SM [%]")
+    algorithm_list.append((k, custom_metric_1))
+
+total_sum = sum([a[1] for a in algorithm_list])
+sorted_list = sorted(algorithm_list, key=lambda x: x[1])
+sorted_list.reverse()
+
+with open(args.output_filename, "w") as f:
+    for a in sorted_list:
+        f.write(f"{a[0]}, {100.0 * a[1] / total_sum}\n")
