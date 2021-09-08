b454524c7d08bce3a25cbc4bd516f7b6baa580d5
Gitlab CI <noreply@cern.ch>
2021-09-08 03:15:14 +0000

    Fixed formatting
    patch generated by https://gitlab.cern.ch/lhcb/Allen/-/jobs/16158925

--- Rec/Allen/src/AllenSVsToRecVertexV2.cpp
+++ Rec/Allen/src/AllenSVsToRecVertexV2.cpp
@@ -31,7 +31,8 @@
 #include "VertexDefinitions.cuh"
 
 class AllenSVsToRecVertexV2 final
-  : public Gaudi::Functional::Transformer<LHCb::Event::v2::RecVertices(const HostBuffers&, const std::vector<LHCb::Event::v2::Track>&)> {
+  : public Gaudi::Functional::Transformer<
+      LHCb::Event::v2::RecVertices(const HostBuffers&, const std::vector<LHCb::Event::v2::Track>&)> {
 public:
   // Standard constructor
   AllenSVsToRecVertexV2(const std::string& name, ISvcLocator* pSvcLocator);
@@ -40,7 +41,8 @@ public:
   StatusCode initialize() override;
 
   // Algorithm execution
-  LHCb::Event::v2::RecVertices operator()(const HostBuffers&, const std::vector<LHCb::Event::v2::Track>&) const override;
+  LHCb::Event::v2::RecVertices operator()(const HostBuffers&, const std::vector<LHCb::Event::v2::Track>&)
+    const override;
 };
 
 #endif
@@ -52,8 +54,7 @@ AllenSVsToRecVertexV2::AllenSVsToRecVertexV2(const std::string& name, ISvcLocato
     name,
     pSvcLocator,
     // Inputs
-    {KeyValue{"AllenOutput", "Allen/Out/HostBuffers"},
-     KeyValue{"OutputTracks", "Allen/Out/ForwardTracks"}},
+    {KeyValue {"AllenOutput", "Allen/Out/HostBuffers"}, KeyValue {"OutputTracks", "Allen/Out/ForwardTracks"}},
     // Outputs
     {KeyValue {"OutputSVs", "Allen/Out/RecVertex"}})
 {}
@@ -65,33 +66,31 @@ StatusCode AllenSVsToRecVertexV2::initialize()
 }
 
 LHCb::Event::v2::RecVertices AllenSVsToRecVertexV2::operator()(
-    const HostBuffers& host_buffers, 
-    const std::vector<LHCb::Event::v2::Track>& tracks) const
+  const HostBuffers& host_buffers,
+  const std::vector<LHCb::Event::v2::Track>& tracks) const
 {
-    const unsigned i_event = 0;
-    const unsigned n_svs = host_buffers.host_number_of_svs[i_event];
+  const unsigned i_event = 0;
+  const unsigned n_svs = host_buffers.host_number_of_svs[i_event];
 
-    LHCb::Event::v2::RecVertices sv_container;
-    sv_container.reserve(n_svs);
+  LHCb::Event::v2::RecVertices sv_container;
+  sv_container.reserve(n_svs);
 
-    for (unsigned int i = 0; i < n_svs; i++) {
-        const VertexFit::TrackMVAVertex& sv = host_buffers.host_secondary_vertices[i];
-        Gaudi::SymMatrix3x3 poscov;
-        poscov(0, 0) = sv.cov00;
-        poscov(1, 0) = sv.cov10;
-        poscov(1, 1) = sv.cov11;
-        poscov(2, 0) = sv.cov20;
-        poscov(2, 1) = sv.cov21;
-        poscov(2, 2) = sv.cov22;
-        Gaudi::XYZPoint position {sv.x, sv.y, sv.z};
-        auto& new_sv = sv_container.emplace_back(    
-            position, poscov, LHCb::Event::v2::Track::Chi2PerDoF {sv.chi2/2, 2});
-        const unsigned i_trackA = sv.trk1;
-        const unsigned i_trackB = sv.trk2;
-        new_sv.addToTracks(&tracks[i_trackA], 0.f);
-        new_sv.addToTracks(&tracks[i_trackB], 0.f);
-    }
+  for (unsigned int i = 0; i < n_svs; i++) {
+    const VertexFit::TrackMVAVertex& sv = host_buffers.host_secondary_vertices[i];
+    Gaudi::SymMatrix3x3 poscov;
+    poscov(0, 0) = sv.cov00;
+    poscov(1, 0) = sv.cov10;
+    poscov(1, 1) = sv.cov11;
+    poscov(2, 0) = sv.cov20;
+    poscov(2, 1) = sv.cov21;
+    poscov(2, 2) = sv.cov22;
+    Gaudi::XYZPoint position {sv.x, sv.y, sv.z};
+    auto& new_sv = sv_container.emplace_back(position, poscov, LHCb::Event::v2::Track::Chi2PerDoF {sv.chi2 / 2, 2});
+    const unsigned i_trackA = sv.trk1;
+    const unsigned i_trackB = sv.trk2;
+    new_sv.addToTracks(&tracks[i_trackA], 0.f);
+    new_sv.addToTracks(&tracks[i_trackB], 0.f);
+  }
 
-    return sv_container;
-
+  return sv_container;
 }
