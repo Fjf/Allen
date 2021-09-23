a40eb0631301b94de619cd6d361dafcb4971d62d
Lorenzo Pica <lorenzo.pica@cern.ch>
2021-09-23 17:47:21 +0200

    Add retina cluster scifi v6 sequence and rename retina v6 validation sequence

--- configuration/sequences/hlt1_pp_validation_retina_cluster_scifi_v6.py
+++ configuration/sequences/hlt1_pp_validation_retina_cluster_scifi_v6.py
@@ -1,6 +1,6 @@
 ###############################################################################
-# (c) Copyright 2021 CERN for the benefit of the LHCb Collaboration           #                                        
-###############################################################################                                                     
+# (c) Copyright 2021 CERN for the benefit of the LHCb Collaboration           #
+###############################################################################
 from AllenConf.HLT1 import setup_hlt1_node
 from AllenConf.velo_reconstruction import decode_velo
 from AllenCore.event_list_utils import generate
@@ -8,5 +8,5 @@ from AllenConf.scifi_reconstruction import decode_scifi
 
 with decode_scifi.bind(raw_bank_version="v6"),\
      decode_velo.bind(retina_decoding="True"):
-    hlt1_node = setup_hlt1_node()
+    hlt1_node = setup_hlt1_node(withMCChecking=True)
 generate(hlt1_node)
