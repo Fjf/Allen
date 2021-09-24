1339725bd4618c91603d81a3d89bdc2f2f5b079d
Roel Aaij <roel.aaij@cern.ch>
2021-09-24 13:34:57 +0200

    Fix out-of-bounds access in UT rawbank v3

--- device/UT/common/include/UTRaw.cuh
+++ device/UT/common/include/UTRaw.cuh
@@ -9,71 +9,50 @@ template<int decoding_version>
 struct UTRawBank {
   uint32_t sourceID;
   std::array<uint32_t, 6> number_of_hits {0, 0, 0, 0, 0, 0};
-  uint16_t* data;
+  const uint16_t* data = nullptr;
 
   static_assert(decoding_version == -1 || decoding_version == 3 || decoding_version == 4);
 
-  __device__ __host__ UTRawBank(const char* ut_raw_bank, const uint32_t size)
-  {
-    uint32_t* p = (uint32_t*) ut_raw_bank;
-    sourceID = *p;
-    p += 1;
-    if constexpr (decoding_version == 3) {
-      number_of_hits[0] = *p & 0x0000FFFFU;
-    }
-    else if constexpr (decoding_version == 4) {
-      bool bad = false;
-      auto add_to_hits = [this, &bad](uint32_t&& n_hits_in_lane, uint32_t&& lane_index) {
-        if (n_hits_in_lane == 255)
-          bad = true;
-        else
-          number_of_hits[lane_index] = n_hits_in_lane;
-      };
-      add_to_hits((*p & 0xFFU) >> 0U, 4U);
-      add_to_hits((*p & 0xFF00U) >> 8U, 5U);
-      p += 1;
-      add_to_hits((*p & 0xFFU) >> 0U, 0U);
-      add_to_hits((*p & 0xFF00U) >> 8U, 1U);
-      add_to_hits((*p & 0xFF0000U) >> 16U, 2U);
-      add_to_hits((*p & 0xFF000000U) >> 24U, 3U);
-      // the header contains garbage if there are actually no words to decode (and there are always 6 words -- the word
-      // is 0 if there are no hits in the lane)
-      // also protect against corrupt events
-      if (((size - 1 * sizeof(uint32_t)) < sizeof(uint32_t) * 6) || bad) number_of_hits = {0, 0, 0, 0, 0, 0};
-    }
-    p += 1;
-    data = (uint16_t*) p;
-  }
+  __device__ __host__ UTRawBank(const char* ut_raw_bank, const uint32_t size) :
+    UTRawBank {reinterpret_cast<const uint32_t*>(ut_raw_bank)[0], ut_raw_bank + sizeof(uint32_t), ut_raw_bank + size}
+  {}
 
   __device__ __host__
   UTRawBank(const uint32_t sID, const char* ut_fragment, [[maybe_unused]] const char* ut_fragment_end)
   {
     sourceID = sID;
-    uint32_t* p = (uint32_t*) ut_fragment;
+    auto p = reinterpret_cast<const uint32_t*>(ut_fragment);
     if constexpr (decoding_version == 3) {
       number_of_hits[0] = *p & 0x0000FFFFU;
+      p += 1;
+      data = reinterpret_cast<const uint16_t*>(p);
     }
     else if constexpr (decoding_version == 4) {
-      bool bad = false;
-      auto add_to_hits = [this, &bad](uint32_t&& n_hits_in_lane, uint32_t&& lane_index) {
-        if (n_hits_in_lane == 255)
-          bad = true;
-        else
-          number_of_hits[lane_index] = n_hits_in_lane;
-      };
-      add_to_hits((*p & 0xFFU) >> 0U, 4U);
-      add_to_hits((*p & 0xFF00U) >> 8U, 5U);
-      p += 1;
-      add_to_hits((*p & 0xFFU) >> 0U, 0U);
-      add_to_hits((*p & 0xFF00U) >> 8U, 1U);
-      add_to_hits((*p & 0xFF0000U) >> 16U, 2U);
-      add_to_hits((*p & 0xFF000000U) >> 24U, 3U);
+      if ((ut_fragment_end - ut_fragment) >= static_cast<long>(6 * sizeof(uint32_t))) {
+        bool bad = false;
+        auto add_to_hits = [this, &bad](uint32_t n_hits_in_lane, uint32_t lane_index) {
+          if (n_hits_in_lane == 255)
+            bad = true;
+          else
+            number_of_hits[lane_index] = n_hits_in_lane;
+        };
+        add_to_hits((*p & 0xFFU) >> 0U, 4U);
+        add_to_hits((*p & 0xFF00U) >> 8U, 5U);
+        p += 1;
+        add_to_hits((*p & 0xFFU) >> 0U, 0U);
+        add_to_hits((*p & 0xFF00U) >> 8U, 1U);
+        add_to_hits((*p & 0xFF0000U) >> 16U, 2U);
+        add_to_hits((*p & 0xFF000000U) >> 24U, 3U);
 
-      if (((ut_fragment_end - ut_fragment) < static_cast<long>(6 * sizeof(uint32_t))) || bad)
-        number_of_hits = {0, 0, 0, 0, 0, 0};
+        if (bad) {
+          number_of_hits = {0, 0, 0, 0, 0, 0};
+        }
+        else {
+          p += 1;
+          data = reinterpret_cast<const uint16_t*>(p);
+        }
+      }
     }
-    p += 1;
-    data = (uint16_t*) p;
   }
 
   __device__ __host__ uint32_t get_n_hits() const
@@ -85,17 +64,17 @@ struct UTRawBank {
 
 struct UTRawEvent {
   uint32_t number_of_raw_banks;
-  uint32_t* raw_bank_offsets;
-  char* data;
+  const uint32_t* raw_bank_offsets;
+  const char* data;
 
   __device__ __host__ UTRawEvent(const char* event)
   {
     const char* p = event;
-    number_of_raw_banks = *((uint32_t*) p);
+    number_of_raw_banks = *(reinterpret_cast<const uint32_t*>(p));
     p += sizeof(uint32_t);
-    raw_bank_offsets = (uint32_t*) p;
+    raw_bank_offsets = reinterpret_cast<const uint32_t*>(p);
     p += (number_of_raw_banks + 1) * sizeof(uint32_t);
-    data = (char*) p;
+    data = p;
   }
 
   template<int decoding_version>
