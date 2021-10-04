297fdef54db698845f7c34d5fc628d699508beb5
gbassi <gbassi@n4050101.lbdaq.cern.ch>
2021-10-04 17:17:06 +0200

    Rebase and add hlt1_pp_validation_retina_cluster sequence

---
+++ configuration/sequences/hlt1_pp_validation_retina_cluster.py
@@ -0,0 +1,10 @@
+###############################################################################
+# (c) Copyright 2021 CERN for the benefit of the LHCb Collaboration           #
+###############################################################################
+from AllenConf.HLT1 import setup_hlt1_node
+from AllenConf.velo_reconstruction import decode_velo
+from AllenCore.event_list_utils import generate
+
+with decode_velo.bind(retina_decoding="True"):
+    hlt1_node = setup_hlt1_node(withMCChecking=True)
+generate(hlt1_node)
