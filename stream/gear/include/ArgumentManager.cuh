3a804bd1ece41b8bfca9a53ffbad83d3dda91010
Daniel Campora <dcampora@cern.ch>
2021-10-12 11:13:03 +0200

    Use void* in ArgumentData.

--- stream/gear/include/ArgumentManager.cuh
+++ stream/gear/include/ArgumentManager.cuh
@@ -28,7 +28,7 @@ struct ArgumentData {
 private:
   std::string m_name = "";
   ArgumentScope m_scope = ArgumentScope::Invalid;
-  char* m_pointer = nullptr;
+  void* m_pointer = nullptr;
   size_t m_size = 0;
   size_t m_type_size = 0;
 
@@ -38,12 +38,12 @@ public:
   ArgumentData(const std::string& name) : m_name(name) {}
   ArgumentData(const std::string& name, ArgumentScope scope) : m_name(name), m_scope(scope) {}
 
-  virtual char* pointer() const { return m_pointer; }
+  virtual void* pointer() const { return m_pointer; }
   virtual size_t size() const { return m_size; }
   virtual size_t sizebytes() const { return m_size * m_type_size; }
   virtual std::string name() const { return m_name; }
   virtual ArgumentScope scope() const { return m_scope; }
-  virtual void set_pointer(char* pointer) { m_pointer = pointer; }
+  virtual void set_pointer(void* pointer) { m_pointer = pointer; }
   virtual void set_size(size_t size) { m_size = size; }
   virtual void set_type_size(size_t type_size) { m_type_size = type_size; }
   virtual void set_name(const std::string& name) { m_name = name; }
