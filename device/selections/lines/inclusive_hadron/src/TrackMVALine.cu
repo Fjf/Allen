76ed6cf406b543169660c566fa1ca4893d2cb399
Andre Gunther <p.gunther@cern.ch>
2021-11-25 18:00:52 +0100

    Use Gaudi::Units::MeV in Properties

--- device/selections/lines/inclusive_hadron/src/TrackMVALine.cu
+++ device/selections/lines/inclusive_hadron/src/TrackMVALine.cu
@@ -11,13 +11,16 @@ __device__ bool track_mva_line::track_mva_line_t::select(
   std::tuple<const ParKalmanFilter::FittedTrack&> input)
 {
   const auto& track = std::get<0>(input);
+
   const auto ptShift = (track.pt() - parameters.alpha) / Gaudi::Units::GeV;
+  const auto maxPt_GeV = parameters.maxPt / Gaudi::Units::GeV;
+  const auto minPt_GeV = parameters.minPt / Gaudi::Units::GeV;
+
   const bool decision =
     track.chi2 / track.ndof < parameters.maxChi2Ndof &&
-    ((ptShift > parameters.maxPt && track.ipChi2 > parameters.minIPChi2) ||
-     (ptShift > parameters.minPt && ptShift < parameters.maxPt &&
+    ((ptShift > maxPt_GeV && track.ipChi2 > parameters.minIPChi2) ||
+     (ptShift > minPt_GeV && ptShift < maxPt_GeV &&
       logf(track.ipChi2) > parameters.param1 / ((ptShift - parameters.param2) * (ptShift - parameters.param2)) +
-                             (parameters.param3 / parameters.maxPt) * (parameters.maxPt - ptShift) +
-                             logf(parameters.minIPChi2)));
+                             (parameters.param3 / maxPt_GeV) * (maxPt_GeV - ptShift) + logf(parameters.minIPChi2)));
   return decision;
 }
