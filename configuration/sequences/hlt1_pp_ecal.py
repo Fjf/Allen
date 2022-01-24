142378b7cb4d6dc2392a8b3d59a3930720716a2c
Maxime Schubiger <maxime.schubiger@nikhef.nl>
2022-01-24 13:46:59 +0100

    Put the calo decoding and electron trigger lines back in the default sequence

--- configuration/sequences/hlt1_pp_ecal.py
+++
@@ -1,8 +0,0 @@
-###############################################################################
-# (c) Copyright 2021 CERN for the benefit of the LHCb Collaboration           #
-###############################################################################
-from AllenConf.HLT1_ECAL import setup_hlt1_node
-from AllenCore.generator import generate
-
-hlt1_ecal_node = setup_hlt1_node()
-generate(hlt1_ecal_node)
