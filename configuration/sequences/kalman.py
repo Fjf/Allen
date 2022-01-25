aef909376fe415bd3a00108ff041c26c19428f70
thboettc <boettcts@ucmail.uc.edu>
2022-01-25 16:13:54 -0500

    Add copyright

--- configuration/sequences/kalman.py
+++ configuration/sequences/kalman.py
@@ -1,3 +1,13 @@
+###############################################################################
+# (c) Copyright 2022 CERN for the benefit of the LHCb Collaboration           #
+#                                                                             #
+# This software is distributed under the terms of the Apache License          #
+# version 2 (Apache-2.0), copied verbatim in the file "COPYING".              #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
 from AllenConf.utils import gec
 from AllenConf.hlt1_reconstruction import hlt1_reconstruction
 from PyConf.control_flow import NodeLogic, CompositeNode
