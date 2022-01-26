ad0250d30f8d85e0c7acacbda4174c95e7469871
Maxime Schubiger <maxime.schubiger@nikhef.nl>
2022-01-26 17:23:14 +0100

    Harmonise name convention

--- configuration/sequences/hlt1_pp_no_calo_validation.py
+++ configuration/sequences/hlt1_pp_no_calo_validation.py
@@ -1,7 +1,7 @@
 ###############################################################################
 # (c) Copyright 2021 CERN for the benefit of the LHCb Collaboration           #
 ###############################################################################
-from AllenConf.HLT1_noCalo import setup_hlt1_node
+from AllenConf.HLT1_no_calo import setup_hlt1_node
 from AllenCore.generator import generate
 
 hlt1_node = setup_hlt1_node(withMCChecking=True)
