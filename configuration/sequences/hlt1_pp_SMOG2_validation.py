9a1636b31d0fa838a5efb1f1f3c81d367b0aa957
Saverio Mariani <samarian@cern.ch>
2022-02-15 17:40:35 +0100

    Add SMOG2 lines testing

--- configuration/sequences/hlt1_pp_SMOG2_validation.py
+++ configuration/sequences/hlt1_pp_SMOG2_validation.py
@@ -5,5 +5,5 @@ from AllenConf.HLT1 import setup_hlt1_node
 from AllenConf.scifi_reconstruction import decode_scifi
 from AllenCore.generator import generate
 
-hlt1_node = setup_hlt1_node(withSMOG2=True, EnableGEC=True)
+hlt1_node = setup_hlt1_node(withMCChecking=True, withSMOG2=True, EnableGEC=True)
 generate(hlt1_node)
