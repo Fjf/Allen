0fa28b56f8ff3147d81e72f0d796064fb05fe6f2
thboettc <boettcts@ucmail.uc.edu>
2022-02-22 14:36:53 -0500

    Make particle container optional in dummy algorithm

--- device/selections/Hlt1/include/ParticleContainerLifeSupport.cuh
+++ device/selections/Hlt1/include/ParticleContainerLifeSupport.cuh
@@ -15,7 +15,7 @@
 
 namespace particle_container_life_support {
   struct Parameters {
-    DEVICE_INPUT(dev_particle_container_ptr_t, Allen::Views::Physics::IMultiEventParticleContainer*)
+    DEVICE_INPUT_OPTIONAL(dev_particle_container_ptr_t, Allen::Views::Physics::IMultiEventParticleContainer*)
     dev_particle_container_ptr_t;
     DEVICE_INPUT(dev_particle_container_user_t, unsigned) dev_particle_container_user_t;
   };
