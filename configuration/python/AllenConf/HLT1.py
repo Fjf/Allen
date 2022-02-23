82cb59c877fd890ac9e23a756e384706ee297f5c
Roel Aaij <raaij@nikhef.nl>
2022-02-23 21:00:46 +0100

    Fix sequence generation warnings introduced with Allen!725

--- configuration/python/AllenConf/HLT1.py
+++ configuration/python/AllenConf/HLT1.py
@@ -77,10 +77,8 @@ def default_physics_lines(velo_tracks, forward_tracks, kalman_velo_only,
         line_maker("Hlt1DiMuonHighMass",
                    make_di_muon_mass_line(forward_tracks, secondary_vertices)))
     lines.append(
-        line_maker(
-            "Hlt1TwoKs",
-            make_two_ks_line(forward_tracks, secondary_vertices),
-            enableGEC=True))
+        line_maker("Hlt1TwoKs",
+                   make_two_ks_line(forward_tracks, secondary_vertices)))
     lines.append(
         line_maker(
             "Hlt1DiMuonLowMass",
