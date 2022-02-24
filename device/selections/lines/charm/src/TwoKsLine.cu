2ce1e3d21875232d2260d9e226c5b003b9c99b9f
thboettc <boettcts@ucmail.uc.edu>
2022-02-23 20:36:18 -0500

    Try to fix MI100 run by separating cuts in TwoKsLine

--- device/selections/lines/charm/src/TwoKsLine.cu
+++ device/selections/lines/charm/src/TwoKsLine.cu
@@ -39,12 +39,16 @@ __device__ bool two_ks_line::two_ks_line_t::select(
   bool dec1 = vertex1.vertex().chi2() > 0 && vertex1.vertex().chi2() < parameters.maxVertexChi2;
   if (!dec1) return false;
   // Kinematic cuts.
-  dec1 &= vertex1.minpt() > parameters.minTrackPt_piKs && vertex1.minp() > parameters.minTrackP_piKs;
-  dec1 &= vertex1.mdipi() > parameters.minM_Ks && vertex1.mdipi() < parameters.maxM_Ks;
+  dec1 &= vertex1.minpt() > parameters.minTrackPt_piKs;
+  dec1 &= vertex1.minp() > parameters.minTrackP_piKs;
+  dec1 &= vertex1.mdipi() > parameters.minM_Ks;
+  dec1 &= vertex1.mdipi() < parameters.maxM_Ks;
   if (!dec1) return false;
   // PV cuts.
-  dec1 &= vertex1.eta() > parameters.minEta_Ks && vertex1.eta() < parameters.maxEta_Ks;
-  dec1 &= vertex1.minipchi2() > parameters.minTrackIPChi2_Ks && vertex1.dira() > parameters.minCosDira;
+  dec1 &= vertex1.eta() > parameters.minEta_Ks;
+  dec1 &= vertex1.eta() < parameters.maxEta_Ks;
+  dec1 &= vertex1.minipchi2() > parameters.minTrackIPChi2_Ks;
+  dec1 &= vertex1.dira() > parameters.minCosDira;
   if (!dec1) return false;
   // Cuts that need constituent tracks.
   const auto v1track1 = static_cast<const Allen::Views::Physics::BasicParticle*>(vertex1.substructure(0));
@@ -53,7 +57,10 @@ __device__ bool two_ks_line::two_ks_line_t::select(
     (v1track1->px() * v1track2->px() + v1track1->py() * v1track2->py() + v1track1->pz() * v1track2->pz()) /
     (v1track1->p() * v1track2->p());
   dec1 &= cos1 > parameters.minCosOpening;
-  dec1 &= v1track1->ip() * v1track2->ip() / vertex1.ip() > parameters.min_combip;
+  const float v1ip1 = v1track1->ip();
+  const float v1ip2 = v1track2->ip();
+  const float v1ip = vertex1.ip();
+  dec1 &= v1ip1 * v1ip2 / v1ip > parameters.min_combip;
   if (!dec1) return false;
 
   for (unsigned i = threadIdx.y + vertex1_id + 1; i < n_svs; i += blockDim.y) {
@@ -77,12 +84,16 @@ __device__ bool two_ks_line::two_ks_line_t::select(
     bool dec2 = vertex2.vertex().chi2() > 0 && vertex2.vertex().chi2() < parameters.maxVertexChi2;
     if (!dec2) continue;
     // Kinematic cuts.
-    dec2 &= vertex2.minpt() > parameters.minTrackPt_piKs && vertex2.minp() > parameters.minTrackP_piKs;
-    dec2 &= vertex2.mdipi() > parameters.minM_Ks && vertex2.mdipi() < parameters.maxM_Ks;
+    dec2 &= vertex2.minpt() > parameters.minTrackPt_piKs;
+    dec2 &= vertex2.minp() > parameters.minTrackP_piKs;
+    dec2 &= vertex2.mdipi() > parameters.minM_Ks;
+    dec2 &= vertex2.mdipi() < parameters.maxM_Ks;
     if (!dec2) continue;
     // PV cuts.
-    dec2 &= vertex2.eta() > parameters.minEta_Ks && vertex2.eta() < parameters.maxEta_Ks;
-    dec2 &= vertex2.minipchi2() > parameters.minTrackIPChi2_Ks && vertex2.dira() > parameters.minCosDira;
+    dec2 &= vertex2.eta() > parameters.minEta_Ks;
+    dec2 &= vertex2.eta() < parameters.maxEta_Ks;
+    dec2 &= vertex2.minipchi2() > parameters.minTrackIPChi2_Ks;
+    dec2 &= vertex2.dira() > parameters.minCosDira;
     if (!dec2) continue;
     // Cuts that need constituent tracks.
     const auto v2track1 = static_cast<const Allen::Views::Physics::BasicParticle*>(vertex2.substructure(0));
@@ -91,8 +102,10 @@ __device__ bool two_ks_line::two_ks_line_t::select(
       (v2track1->px() * v2track2->px() + v2track1->py() * v2track2->py() + v2track1->pz() * v2track2->pz()) /
       (v2track1->p() * v2track2->p());
     dec2 &= cos2 > parameters.minCosOpening;
-    dec2 &= v2track1->ip() * v2track2->ip() / vertex2.ip() > parameters.min_combip;
-    if (!dec2) continue;
+    const float v2ip1 = v2track1->ip();
+    const float v2ip2 = v2track2->ip();
+    const float v2ip = vertex2.ip();
+    dec2 &= v2ip1 * v2ip2 / v2ip > parameters.min_combip;
 
     if (dec1 && dec2) {
       return true;
