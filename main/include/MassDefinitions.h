6fab8fea62c8fc873592c9e6873e99f313e86e77
Dorothea vom Bruch <dorothea.vom.bruch@cern.ch>
2022-03-10 16:45:01 +0100

    add header file with mass definitions

---
+++ main/include/MassDefinitions.h
@@ -0,0 +1,13 @@
+/*****************************************************************************\
+* (c) Copyright 2018-2020 CERN for the benefit of the LHCb Collaboration      *
+\*****************************************************************************/
+
+namespace Allen {
+  
+  constexpr float mEl = 0.51099891;
+  constexpr float mMu = 105.65837;
+  constexpr float mPi = 139.57018;
+  constexpr float mK = 493.677;
+  constexpr float mP = 938.27203;
+  constexpr float mDz = 1864.83;
+}
