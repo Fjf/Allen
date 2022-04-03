25bfb6fe2e9202cec15da7ced2f389e0f7d72a4a
Gitlab CI <noreply@cern.ch>
2022-04-03 20:11:45 +0000

    Fixed formatting
    patch generated by https://gitlab.cern.ch/lhcb/Allen/-/jobs/20812506

--- Rec/Allen/src/AllenPVsToLHCbPVs.cpp
+++ Rec/Allen/src/AllenPVsToLHCbPVs.cpp
@@ -21,10 +21,9 @@
 #include "PV_Definitions.cuh"
 #include "patPV_Definitions.cuh"
 
-using Vertices = LHCb::Event::PV::PrimaryVertexContainer ;
+using Vertices = LHCb::Event::PV::PrimaryVertexContainer;
 
-class AllenPVsToLHCbPVs final
-  : public Gaudi::Functional::Transformer<Vertices(const HostBuffers&)> {
+class AllenPVsToLHCbPVs final : public Gaudi::Functional::Transformer<Vertices(const HostBuffers&)> {
 public:
   /// Standard constructor
   AllenPVsToLHCbPVs(const std::string& name, ISvcLocator* pSvcLocator);
@@ -38,7 +37,6 @@ public:
 private:
   mutable Gaudi::Accumulators::SummingCounter<unsigned int> m_nbPVsCounter {this, "Nb PVs"};
 };
-
 
 DECLARE_COMPONENT(AllenPVsToLHCbPVs)
 
@@ -66,8 +64,8 @@ Vertices AllenPVsToLHCbPVs::operator()(const HostBuffers& host_buffers) const
 
   if (msgLevel(MSG::DEBUG)) debug() << "Number of PVs to convert = " << n_pvs << endmsg;
 
-  Vertices pvcontainer ;
-  auto& vertices = pvcontainer.vertices ;
+  Vertices pvcontainer;
+  auto& vertices = pvcontainer.vertices;
   vertices.reserve(n_pvs);
 
   for (unsigned int i = 0; i < n_pvs; i++) {
@@ -80,12 +78,12 @@ Vertices AllenPVsToLHCbPVs::operator()(const HostBuffers& host_buffers) const
     poscov(2, 0) = vertex.cov20;
     poscov(2, 1) = vertex.cov21;
     poscov(2, 2) = vertex.cov22;
-    auto& recvertex = vertices.emplace_back( Gaudi::XYZPoint{vertex.position.x, vertex.position.y, vertex.position.z} ) ;
-    recvertex.setCovMatrix( poscov ) ;
-    recvertex.setChi2( vertex.chi2 ) ;
-    recvertex.setNDoF( vertex.ndof ) ;
+    auto& recvertex = vertices.emplace_back(Gaudi::XYZPoint {vertex.position.x, vertex.position.y, vertex.position.z});
+    recvertex.setCovMatrix(poscov);
+    recvertex.setChi2(vertex.chi2);
+    recvertex.setNDoF(vertex.ndof);
   }
 
   m_nbPVsCounter += vertices.size();
-  return pvcontainer ;
+  return pvcontainer;
 }
