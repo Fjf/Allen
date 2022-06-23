1eea3046ae47446d388dd29ca30f25f18d1439f5
Gerhard Raven <gerhard.raven@nikhef.nl>
2022-06-23 10:31:31 +0200

    cleanup GaudiAllenReportsToRawEvent implementation

--- Rec/Allen/src/GaudiAllenReportsToRawEvent.cpp
+++ Rec/Allen/src/GaudiAllenReportsToRawEvent.cpp
@@ -8,44 +8,50 @@
 * granted to it by virtue of its status as an Intergovernmental Organization  *
 * or submit itself to any jurisdiction.                                       *
 \*****************************************************************************/
-
-#include "GaudiAllenReportsToRawEvent.h"
-
-// LHCb
+// Gaudi
+#include "GaudiAlg/Transformer.h"
+#include "Event/RawEvent.h"
+#include <vector>
 #include "Kernel/STLExtensions.h"
 
+struct GaudiAllenReportsToRawEvent final : public Gaudi::Functional::Transformer<LHCb::RawEvent(
+                                             const std::vector<unsigned>&,
+                                             const std::vector<unsigned>&,
+                                             const std::vector<unsigned>&,
+                                             const std::vector<unsigned>&)> {
+  // Standard constructor
+  GaudiAllenReportsToRawEvent(const std::string& name, ISvcLocator* pSvcLocator) :
+    Transformer(
+      name,
+      pSvcLocator,
+      // Inputs
+      {KeyValue {"allen_number_of_active_lines", ""},
+       KeyValue {"allen_dec_reports", ""},
+       KeyValue {"allen_selrep_offsets", ""},
+       KeyValue {"allen_sel_reports", ""}},
+      // Outputs
+      {KeyValue {"OutputRawReports", "Allen/Out/RawReports"}})
+  {}
+
+  // Algorithm execution
+  LHCb::RawEvent operator()(
+    const std::vector<unsigned>& allen_number_of_active_lines,
+    const std::vector<unsigned>& allen_dec_reports,
+    const std::vector<unsigned>& allen_selrep_offsets,
+    const std::vector<unsigned>& allen_sel_reports) const override
+  {
+    LHCb::RawEvent raw_event;
+    // TODO: get these hard coded numbers from somewhere else... should be defined in one location only!
+    constexpr auto hlt1SourceID_old = (1u << 13);
+    constexpr auto hlt1SourceID_new = (1u << 8);
+    constexpr auto sel_rep_version = 9u, dec_rep_version = 3u;
+    auto dec_reports = LHCb::span {allen_dec_reports}.first(3 + allen_number_of_active_lines[0]);
+    auto sel_reports = LHCb::span {allen_sel_reports}.first(allen_selrep_offsets[1]);
+    raw_event.addBank(hlt1SourceID_old, LHCb::RawBank::HltSelReports, sel_rep_version, sel_reports);
+    raw_event.addBank(hlt1SourceID_new, LHCb::RawBank::HltDecReports, dec_rep_version, dec_reports);
+
+    return raw_event;
+  }
+};
+
 DECLARE_COMPONENT(GaudiAllenReportsToRawEvent)
-
-GaudiAllenReportsToRawEvent::GaudiAllenReportsToRawEvent(const std::string& name, ISvcLocator* pSvcLocator) :
-  Transformer(
-    name,
-    pSvcLocator,
-    // Inputs
-    {KeyValue {"allen_number_of_active_lines", ""},
-     KeyValue {"allen_dec_reports", ""},
-     KeyValue {"allen_selrep_offsets", ""},
-     KeyValue {"allen_sel_reports", ""}},
-    // Outputs
-    {KeyValue {"OutputRawReports", "Allen/Out/RawReports"}})
-{}
-
-LHCb::RawEvent GaudiAllenReportsToRawEvent::operator()(
-  const std::vector<unsigned>& allen_number_of_active_lines,
-  const std::vector<unsigned>& allen_dec_reports,
-  const std::vector<unsigned>& allen_selrep_offsets,
-  const std::vector<unsigned>& allen_sel_reports) const
-{
-
-  LHCb::RawEvent raw_event;
-  // TODO: get these hard coded numbers from somewhere else... should be defined in one location only!
-  constexpr auto hlt1SourceID_old = (1u << 13);
-  constexpr auto hlt1SourceID_new = (1u << 8);
-  constexpr auto sel_rep_version = 9u, dec_rep_version = 3u;
-  auto dec_reports =
-    LHCb::make_span(&allen_dec_reports[0], &allen_dec_reports[allen_number_of_active_lines[0] + 3] + 1);
-  auto sel_reports = LHCb::make_span(&allen_sel_reports[0], allen_selrep_offsets[1]);
-  raw_event.addBank(hlt1SourceID_old, LHCb::RawBank::HltSelReports, sel_rep_version, sel_reports);
-  raw_event.addBank(hlt1SourceID_new, LHCb::RawBank::HltDecReports, dec_rep_version, dec_reports);
-
-  return raw_event;
-}
