54f43ff2a0e5e9e36a825e8f4b3a1456f16d45b9
rta <kate.abigail.richardson@cern.ch>
2022-06-29 16:27:39 +0200

    make compile

--- configuration/AllenCore/AllenSequenceGenerator.py
+++ configuration/AllenCore/AllenSequenceGenerator.py
@@ -3,7 +3,6 @@
 ###############################################################################
 from collections import OrderedDict
 from PyConf.dataflow import GaudiDataHandle
-from AllenAlgorithms.algorithms import AlgorithmCategory
 from json import dump
 
 
