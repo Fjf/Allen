896aa26fcb9fd5c9a325757d5ba53d4c32d0fa5f
Saverio Mariani <samarian@cern.ch>
2022-07-08 18:54:12 +0200

    Undo changes to MuonRaw.cuh

--- device/event_model/muon/include/MuonRaw.cuh
+++ device/event_model/muon/include/MuonRaw.cuh
@@ -22,13 +22,11 @@ namespace Muon {
 
     __device__ MuonRawBank(const char* raw_bank, const uint16_t s)
     {
-
       const char* p = raw_bank;
       sourceID = reinterpret_cast<const uint32_t*>(p)[0];
       p += sizeof(uint32_t);
       data = reinterpret_cast<const data_type*>(p);
       last = reinterpret_cast<const data_type*>(p + s);
-
     }
 
     __device__ MuonRawBank(const uint32_t sID, const char* bank_start, const uint16_t s, const uint8_t t)
@@ -51,10 +49,10 @@ namespace Muon {
     __device__ __host__ void initialize(const char* event, const uint16_t* sizes)
     {
       const char* p = event;
-      m_number_of_raw_banks = *reinterpret_cast<const uint32_t*>(p); //nBanks
+      m_number_of_raw_banks = *reinterpret_cast<const uint32_t*>(p);
       p += sizeof(uint32_t);
       m_raw_bank_offset = reinterpret_cast<const uint32_t*>(p);
-      p += (m_number_of_raw_banks + 1) * sizeof(uint32_t); //we have the 0
+      p += (m_number_of_raw_banks + 1) * sizeof(uint32_t);
       m_payload = p;
       m_raw_bank_sizes = sizes;
     }
@@ -78,7 +76,7 @@ namespace Muon {
     __device__ __host__ unsigned number_of_raw_banks() const { return m_number_of_raw_banks; }
 
     __device__ __host__ MuonRawBank<version> raw_bank(const unsigned index) const
-    {      
+    {
       return MuonRawBank<version> {m_payload + m_raw_bank_offset[index], m_raw_bank_sizes[index]};
     }
   };
