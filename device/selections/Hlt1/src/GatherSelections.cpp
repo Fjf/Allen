3409de00eccf483e994cd213b86422ea2c296db1
Kate Abigail Richardson <kate.abigail.richardson@cern.ch>
2022-07-28 12:36:48 +0200

    remove RateVsTime histograms

--- device/selections/Hlt1/src/GatherSelections.cpp
+++ device/selections/Hlt1/src/GatherSelections.cpp
@@ -27,11 +27,8 @@ void gather_selections::gather_selections_t::init_monitor()
   while (std::getline(is, line_name, ',')) {
     const std::string pass_counter_name {line_name + "Pass"};
     const std::string rate_counter_name {line_name + "Rate"};
-    const std::string rate_hist_name {line_name + "RateVsTime"};
     m_pass_counters.push_back(std::make_unique<Gaudi::Accumulators::Counter<>>(this, pass_counter_name));
     m_rate_counters.push_back(std::make_unique<Gaudi::Accumulators::Counter<>>(this, rate_counter_name));
-    histograms_rates_vs_time.emplace_back((void*) new gaudi_histo_t<1>(
-      this, rate_hist_name, rate_hist_name, Gaudi::Accumulators::Axis<float> {100, 0, 1000.}));
     line_labels.push_back(line_name);
   }
   unsigned int n_lines = line_labels.size();
@@ -65,25 +62,18 @@ void gather_selections::gather_selections_t::monitor_operator(
 
 void gather_selections::gather_selections_t::monitor_postscaled_operator(
   const ArgumentReferences<Parameters>& arguments,
-  const Constants& constants,
+  const Constants&,
   gsl::span<bool> postscaled_decisions) const
 {
   auto* histogram_line_rates_p = reinterpret_cast<gaudi_histo_t<1>*>(histogram_line_rates);
   auto hist_buf = histogram_line_rates_p->buffer();
-  auto time_bin = (std::chrono::duration_cast<std::chrono::duration<float>>(
-                     std::chrono::high_resolution_clock::now() - constants.histogram_time_epoch))
-                    .count() /
-                  10.f;
   // Fill postscaled counters and histograms
   for (auto j = 0u; j < first<host_number_of_active_lines_t>(arguments); ++j) {
     auto buf = m_rate_counters[j]->buffer();
-    auto* histograms_rates_vs_time_p = reinterpret_cast<gaudi_histo_t<1>*>(histograms_rates_vs_time[j]);
-    auto buf_time = histograms_rates_vs_time_p->buffer();
     for (auto i = 0u; i < first<host_number_of_events_t>(arguments); ++i) {
       if (postscaled_decisions[i * first<host_number_of_active_lines_t>(arguments) + j]) {
         ++buf;
         ++hist_buf[j];
-        ++buf_time[time_bin];
       }
     }
   }
