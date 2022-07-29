18d9d721adae9509b16d3d6f7ad17c5bb586fa43
Vladimir Gligorov <vladimir.gligorov@cern.ch>
2022-07-29 10:40:43 +0200

    Update configuration/python/AllenSequences/hlt1_pp_no_gec_no_ut.py, configuration/python/AllenSequences/hlt1_pp_no_gec_no_ut_validation.py, scripts/ci/config/common-run.yaml

---
+++ configuration/python/AllenSequences/hlt1_pp_no_gec_no_ut_validation.py
@@ -0,0 +1,9 @@
+###############################################################################
+# (c) Copyright 2021 CERN for the benefit of the LHCb Collaboration           #
+###############################################################################
+from AllenConf.HLT1 import setup_hlt1_node
+from AllenCore.generator import generate
+
+hlt1_node = setup_hlt1_node(
+    with_ut=False, withMCChecking=True, EnableGEC=False, enableRateValidator=True)
+generate(hlt1_node)
