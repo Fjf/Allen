4814ae8bc05b4ea446528c6156aaa79ed043c2d1
Gitlab CI <noreply@cern.ch>
2022-07-29 08:45:40 +0000

    Fixed formatting
    patch generated by https://gitlab.cern.ch/lhcb/Allen/-/jobs/23631509

--- configuration/python/AllenSequences/hlt1_pp_no_gec_no_ut.py
+++ configuration/python/AllenSequences/hlt1_pp_no_gec_no_ut.py
@@ -4,5 +4,6 @@
 from AllenConf.HLT1 import setup_hlt1_node
 from AllenCore.generator import generate
 
-hlt1_node = setup_hlt1_node(with_ut=False, EnableGEC=False, enableRateValidator=True)
+hlt1_node = setup_hlt1_node(
+    with_ut=False, EnableGEC=False, enableRateValidator=True)
 generate(hlt1_node)
