59ee902c18884351eef633a281bd78c28f8682d0
Roel Aaij <raaij@nikhef.nl>
2022-08-03 11:35:56 +0200

    Update DumpBeamline to use DeVP to get the beam spot location

--- Dumpers/BinaryDumpers/src/DumpBeamline.cpp
+++ Dumpers/BinaryDumpers/src/DumpBeamline.cpp
@@ -4,8 +4,8 @@
 #include <tuple>
 #include <vector>
 
-#include <yaml-cpp/yaml.h>
 #include <DetDesc/GenericConditionAccessorHolder.h>
+#include <VPDet/DeVP.h>
 
 #include <Dumpers/Identifiers.h>
 #include <Dumpers/Utils.h>
@@ -14,19 +14,18 @@
 #include "Dumper.h"
 
 namespace {
-  inline const std::string beamSpotCond = "/dd/Conditions/Online/Velo/MotionSystem";
+  struct Beamline {
 
-  struct Beamline_t {
-    double X = std::numeric_limits<double>::signaling_NaN();
-    double Y = std::numeric_limits<double>::signaling_NaN();
+    Beamline() {}
 
-    Beamline_t() {}
-
-    Beamline_t(std::vector<char>& data, YAML::Node const& n) :
-      X {(n["ResolPosRC"].as<double>() + n["ResolPosLA"].as<double>()) / 2}, Y {n["ResolPosY"].as<double>()}
+    Beamline(std::vector<char>& data, DeVP const& velo)
     {
       DumpUtils::Writer output;
-      output.write(X, Y);
+
+      auto const beamSpot = velo.beamSpot();
+      float x = static_cast<float>(beamSpot.x());
+      float y = static_cast<float>(beamSpot.y());
+      output.write(x, y);
       data = output.buffer();
     }
   };
@@ -39,11 +38,11 @@ namespace {
  *  @date   2019-04-27
  */
 class DumpBeamline final
-  : public Allen::Dumpers::Dumper<void(Beamline_t const&), LHCb::DetDesc::usesConditions<Beamline_t>> {
+  : public Allen::Dumpers::Dumper<void(Beamline const&), LHCb::DetDesc::usesConditions<Beamline>> {
 public:
   DumpBeamline(const std::string& name, ISvcLocator* svcLoc);
 
-  void operator()(const Beamline_t& beamline) const override;
+  void operator()(const Beamline& beamline) const override;
 
   StatusCode initialize() override;
 
@@ -54,19 +53,19 @@ private:
 DECLARE_COMPONENT(DumpBeamline)
 
 DumpBeamline::DumpBeamline(const std::string& name, ISvcLocator* svcLoc) :
-  Dumper(name, svcLoc, {KeyValue {"BeamSpotLocation", "AlgorithmSpecific-" + name + "-beamspot"}})
+  Dumper(name, svcLoc, {KeyValue {"BeamSpotLocation", location(name, "beamspot")}})
 {}
 
 StatusCode DumpBeamline::initialize()
 {
   return Dumper::initialize().andThen([&] {
     register_producer(Allen::NonEventData::Beamline::id, "beamline", m_data);
-    addConditionDerivation({beamSpotCond}, inputLocation<Beamline_t>(), [&](YAML::Node const& n) {
-      auto beamline = Beamline_t {m_data, n};
+    addConditionDerivation({DeVPLocation::Default}, inputLocation<Beamline>(), [&](DeVP const& velo) {
+      auto beamline = Beamline {m_data, velo};
       dump();
       return beamline;
     });
   });
 }
 
-void DumpBeamline::operator()(const Beamline_t&) const {}
+void DumpBeamline::operator()(const Beamline&) const {}
