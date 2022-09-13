872548850fe62bca8fa1edab8456b77130c092a6
Roel Aaij <raaij@nikhef.nl>
2022-09-13 12:16:18 +0200

    Add HLT1 sequences based only only on (velo+scifi) tracking

---
+++ configuration/python/AllenSequences/hlt1_pp_only_matching.py
@@ -0,0 +1,9 @@
+###############################################################################
+# (c) Copyright 2021 CERN for the benefit of the LHCb Collaboration           #
+###############################################################################
+from AllenConf.HLT1 import setup_hlt1_node
+from AllenCore.generator import generate
+
+hlt1_node = setup_hlt1_node(matching=True, with_calo=False,
+                            with_muon=False, with_ut=False)
+generate(hlt1_node)
