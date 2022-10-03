90980e8a68224d4493ee14863ead07fdca1b5790
Gitlab CI <noreply@cern.ch>
2022-10-03 12:14:33 +0000

    Fixed formatting
    patch generated by https://gitlab.cern.ch/lhcb/Allen/-/jobs/24921263

--- configuration/python/AllenCore/configuration_options.py
+++ configuration/python/AllenCore/configuration_options.py
@@ -15,8 +15,16 @@ class AllenConfigurationOptions:
     def __init__(self):
         from argparse import ArgumentParser, BooleanOptionalAction
         parser = ArgumentParser()
-        parser.add_argument("--standalone", action="store_true", dest="standalone", default=False)
-        parser.add_argument("--no-register-keys", action="store_false", dest="register_keys", default=True)
+        parser.add_argument(
+            "--standalone",
+            action="store_true",
+            dest="standalone",
+            default=False)
+        parser.add_argument(
+            "--no-register-keys",
+            action="store_false",
+            dest="register_keys",
+            default=True)
         args, _ = parser.parse_known_args()
         self.standalone = args.standalone
         self.register_keys = args.register_keys
