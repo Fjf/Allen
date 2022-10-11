54715464460e489bb33e264d67c1e71e8f7ce825
Daniel Campora <dcampora@cern.ch>
2022-10-11 18:06:53 +0200

    Add copyright.

--- configuration/python/AllenCore/gen_allen_json.py
+++ configuration/python/AllenCore/gen_allen_json.py
@@ -1,3 +1,13 @@
+###############################################################################
+# (c) Copyright 2022 CERN for the benefit of the LHCb Collaboration           #
+#                                                                             #
+# This software is distributed under the terms of the Apache License          #
+# version 2 (Apache-2.0), copied verbatim in the file "COPYING".              #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
 if __name__ == "__main__":
     from argparse import ArgumentParser
     parser = ArgumentParser()
