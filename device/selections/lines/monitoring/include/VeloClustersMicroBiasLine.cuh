0a1326145df4cf5978df9337e3d2b3490c126878
Patrick Spradlin <patrick.spradlin@cern.ch>
2022-10-14 10:37:36 +0100

    Added new line algorithm that selects events based on minimum number of VELO clusters

---
+++ device/selections/lines/monitoring/include/VeloClustersMicroBiasLine.cuh
@@ -0,0 +1,46 @@
+/*****************************************************************************\
+* (c) Copyright 2022 CERN for the benefit of the LHCb Collaboration           *
+\*****************************************************************************/
+#pragma once
+
+#include "AlgorithmTypes.cuh"
+#include "ParticleTypes.cuh"
+#include "EventLine.cuh"
+
+namespace velo_clusters_micro_bias_line {
+  struct Parameters {
+    HOST_INPUT(host_number_of_events_t, unsigned) host_number_of_events;
+    DEVICE_INPUT(dev_number_of_events_t, unsigned) dev_number_of_events;
+    MASK_INPUT(dev_event_list_t) dev_event_list;
+
+    DEVICE_INPUT(dev_offsets_estimated_input_size_t, unsigned) dev_offsets_estimated_input_size;
+
+    HOST_OUTPUT(host_decisions_size_t, unsigned) host_decisions_size;
+    HOST_OUTPUT(host_post_scaler_t, float) host_post_scaler;
+    HOST_OUTPUT(host_post_scaler_hash_t, uint32_t) host_post_scaler_hash;
+
+    HOST_OUTPUT(host_fn_parameters_t, char) host_fn_parameters;
+
+    PROPERTY(pre_scaler_t, "pre_scaler", "Pre-scaling factor", float) pre_scaler;
+    PROPERTY(post_scaler_t, "post_scaler", "Post-scaling factor", float) post_scaler;
+    PROPERTY(pre_scaler_hash_string_t, "pre_scaler_hash_string", "Pre-scaling hash string", std::string);
+    PROPERTY(post_scaler_hash_string_t, "post_scaler_hash_string", "Post-scaling hash string", std::string);
+    PROPERTY(min_velo_clusters_t, "min_velo_clusters", "Minimum number of VELO clusters", unsigned) min_velo_clusters;
+  };
+
+  struct velo_clusters_micro_bias_line_t : public SelectionAlgorithm,
+                                           Parameters,
+                                           EventLine<velo_clusters_micro_bias_line_t, Parameters> {
+    __device__ static std::tuple<const unsigned>
+    get_input(const Parameters& parameters, const unsigned event_number, const unsigned);
+
+    __device__ static bool select(const Parameters& parameters, std::tuple<const unsigned> input);
+
+  private:
+    Property<pre_scaler_t> m_pre_scaler {this, 1.f};
+    Property<post_scaler_t> m_post_scaler {this, 1.f};
+    Property<pre_scaler_hash_string_t> m_pre_scaler_hash_string {this, ""};
+    Property<post_scaler_hash_string_t> m_post_scaler_hash_string {this, ""};
+    Property<min_velo_clusters_t> m_min_velo_clusters {this, 1};
+  };
+} // namespace velo_clusters_micro_bias_line
