7f9fbc3f993597e9fa70f545e5af8eddece99b0c
Daniel Craik <daniel.charles.craik@cern.ch>
2022-12-09 14:09:43 +0100

    fix gpu builds

--- device/lumi/include/LumiCommon.cuh
+++ device/lumi/include/LumiCommon.cuh
@@ -12,5 +12,5 @@
 
 #include <LumiDefinitions.cuh>
 
-__global__ void
+__device__ void
 fillLumiInfo(Lumi::LumiInfo& info, const std::pair<unsigned, unsigned>& offsetAndSize, const unsigned value);
--- device/lumi/src/LumiCommon.cu
+++ device/lumi/src/LumiCommon.cu
@@ -10,7 +10,7 @@
 \*****************************************************************************/
 #include "LumiCommon.cuh"
 
-__global__ void
+__device__ void
 fillLumiInfo(Lumi::LumiInfo& info, const std::pair<unsigned, unsigned>& offsetAndSize, const unsigned value)
 {
   info.size = offsetAndSize.second;
