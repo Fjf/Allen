8397a5ca863e7d65bc15892f4693e26e28eb01b5
Roel Aaij <roel.aaij@cern.ch>
2023-02-27 14:10:44 +0100

    Fix tae filter configuration

--- configuration/python/AllenSequences/hlt1_pp_no_gec_no_ut.py
+++ configuration/python/AllenSequences/hlt1_pp_no_gec_no_ut.py
@@ -5,8 +5,5 @@ from AllenConf.HLT1 import setup_hlt1_node
 from AllenCore.generator import generate
 
 hlt1_node = setup_hlt1_node(
-    with_ut=False,
-    EnableGEC=False,
-    enableRateValidator=True,
-    tae_passthrough=True)
+    with_ut=False, EnableGEC=False, enableRateValidator=True)
 generate(hlt1_node)
